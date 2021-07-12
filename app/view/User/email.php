7804051ce6cd5c2cf7f0d8fce829999bf29c593a
Vitaliy <vvoronik@yandex.ru>
2021-07-12 19:16:37 +0300

    гunsubscribe

--- app/view/User/email.php
+++ app/view/User/email.php
@@ -1,15 +1,15 @@
 <html>
-<body style='font-family:Arial,sans-serif;'>
-<p><strong> Для подтверждения почты перейдите по ссылке</strong>
-	<a style='cursor:pointer;' target="_blank" href=<?=
+	<body style='font-family:Arial,sans-serif;'>
+	<p><strong> Для подтверждения почты перейдите по ссылке</strong>
+		<a style='cursor:pointer;' target="_blank" href=<?=
 <<<here
 "{$_SERVER['REQUEST_SCHEME']}://
 {$_SERVER['SERVER_NAME']}
 /user/confirm?hash={$hash}">
 here; ?>
 Подтверждение почты
-</a>
-</p>
-<a href="https://vitexpopt.ru/unsubscribe?email=<?=$address?>"></a>
+		</a>
+	</p>
+	<a href="https://vitexpopt.ru/unsubscribe?email=<?=$address?>">Отписаться от рассылки</a>
   </body>
 </html></html>
