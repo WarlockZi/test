ff53a3ffe455326335873dfa8a68586d8b0db572
Vitaliy <vvoronik@yandex.ru>
2021-07-15 17:31:26 +0300

    css

--- app/view/layouts/admin.php
+++ app/view/layouts/admin.php
@@ -73,7 +73,7 @@
 			<a href="/adminsc/catalog" class="module catalog"><span>Каталог</span></a>
 			<a href="/adminsc/settings" class="module settings"><span>Настройки</span></a>
 			<a href="/adminsc/crm" class="module crm"><span>CRM</span></a>
-			<a href="/adminsc/test/edit/1" class="module test"><span>Тестирование</span></a>
+			<a href="/adminsc/test/edit/1" class="module test"><span>Тесты</span></a>
 
 		</div>
 
