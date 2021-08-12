c43b6a3eb18b54e692c0e2235e9f95a36a25cc2b
Vitaliy <vvoronik@yandex.ru>
2021-08-12 14:58:24 +0300

    create answer

--- app/view/Test/editBlockQuestion2.php
+++ app/view/Test/editBlockQuestion2.php
@@ -1,3 +1,8 @@
+<? if (!isset($qestion__create)): ?>
+	<div class="question__create">Добавить  вопрос</div>
+<? endif; ?>
+<? $qestion__create = 1 ?>
+
 <div class="question-edit" id="<?= $q_id ?>">
 
 	<div class="question-edit_row">
@@ -12,12 +17,13 @@
 
 		<div class="question__answers">
 
-			<? $i = 0; foreach ($block as $index => $a): ?>
+			<? $i = 0;
+			foreach ($block as $index => $a): ?>
 				<? if ($index): ?>
 
 					<div class="answer">
-						<div class="answer__number"><?= ++$i?></div>
-						<input type="checkbox" class="answer__correct" <?= $a['correct_answer']?'checked':'' ?>>
+						<div class="answer__number"><?= ++$i ?></div>
+						<input type="checkbox" class="answer__correct" <?= $a['correct_answer'] ? 'checked' : '' ?>>
 						<div class="answer__text" contenteditable="true">
 							<?= $a['answer_text'] ?>
 						</div>
@@ -27,6 +33,16 @@
 				<? endif; ?>
 
 			<? endforeach; ?>
+
+
+				<div class="answer__create-button">+</div>
+				<div class="answer__create">
+					<div class="answer__number">№</div>
+					<input type="checkbox" class="answer__correct">
+					<div class="answer__text" contenteditable="true"></div>
+					<div class="answer__delete"><?= include ROOT . '/app/view/components/icons/trashIcon.php'; ?></div>
+
+				</div>
 		</div>
 
 
--- public/src/Test/test_edit_theme_2.js
+++ public/src/Test/test_edit_theme_2.js
@@ -1,1 +1,15 @@
 import './test_edit_theme_2.scss'
+import {_answer} from "./model/answer";
+import {$} from "../common";
+
+$('.answer__create-button').on('click', function (e){
+    let button = e.target
+    let newAnswer = $(button.parentNode).find('.answer__create')
+    let clone = newAnswer.cloneNode(true)
+    clone.style.display = 'flex'
+    newAnswer.after(clone)
+
+    if (_answer.create){
+        let create__answerButton = 0
+    }
+})
