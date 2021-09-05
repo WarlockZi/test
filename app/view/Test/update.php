ea542d46932d249bf18f9a23d0906eb462456b13
Vitaliy <vvoronik@yandex.ru>
2021-09-05 16:33:23 +0300

    update

--- app/view/Test/update.php
+++ app/view/Test/update.php
@@ -1,11 +1,11 @@
 <div class="test-edit-wrapper">
-	<?$t=$test['isTest']?true:false?>
+	<? $t = $test['isTest'] ? true : false ?>
 	<? include ROOT . '/app/view/Test/edit_menu.php' ?>
 
 	<div class="test-edit__content">
-		<div class="test-name">Изменение <?=$t?'теста':'папки'?></div>
+		<div class="test-name">Изменение <?= $t ? 'теста' : 'папки' ?></div>
 		<div class="test-path-add__table">
-			<div>Название <?=$t?'теста':'папки'?></div>
+			<div>Название <?= $t ? 'теста' : 'папки' ?></div>
 			<div id="test_name" class="field" contenteditable="true"><?= $test['test_name'] ?></div>
 			<div>Папка</div>
 
@@ -13,7 +13,7 @@
 				<option value='0'></option>
 				<? foreach ($rootTests as $rootTest): ?>
 					<option value=<?= $rootTest['id'] ?>
-					<?=$rootTest['id']===$test['parent']?'selected':'';?>>
+						<?= $rootTest['id'] === $test['parent'] ? 'selected' : ''; ?>>
 						<?= $rootTest['test_name'] ?>
 					</option>
 				<? endforeach; ?>
@@ -22,8 +22,17 @@
 			<label for="enable">Показыать пользователям</label>
 			<input id="enable" type="checkbox" <?= $test['enable'] ? 'checked' : ''; ?>>
 
-		<div class="test__save">Сохранить</div>
-		<div class="test__delete">Удалить</div>
+			<div class="empty">Содержит Тесты и Папки</div>
+			<? if (isset($test['children'])): ?>
+				<div class="children">
+					<? foreach ($test['children'] as $child): ?>
+						<div class="test-edit__child"><?= $child['test_name'] ?></div>
+					<? endforeach; ?>
+				</div>
+			<? endif; ?>
+
+			<div class="test__save">Сохранить</div>
+			<div class="test__delete">Удалить</div>
 		</div>
 
 	</div>
