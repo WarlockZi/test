af0663f37196049b6f4b2e6383fddee7b980b91f
Vitaliy <vvoronik@yandex.ru>
2021-12-02 17:35:34 +0300

    finished slider menu

--- public/src/components/accordion/accordion.js
+++ public/src/components/accordion/accordion.js
@@ -6,72 +6,90 @@ $('label').on('click', handle)
 function handle(e) {
 
   let checkbox = e.target.previousSibling
-  let parent = checkbox.parentNode.parentNode
+  let parent = checkbox.closest('ul')
+  let ul = checkbox.nextSibling.nextSibling
 
-  // if (checkbox.checked) {
-    let ul = checkbox.nextSibling.nextSibling
-    slideDown(ul, 3, 2 )
+  if (checkbox.checked) {
+    slideUp(ul, 0,)
+  } else {
+    parent.style.height = "auto"
+    slideDown(ul, 0)
+    closeSiblings(parent)
 
-  // } else {
-    Array.from(parent.children).map((el) => {
-        let elArr = Array.from(el.children)
-        elArr.map((ch) => {
-          if (ch.type && ch.type === 'checkbox' && ch.checked) {
-            let ul = ch.nextSibling.nextSibling
-            slideUp(ul, 3, 2, function () {
-              ch.checked = false
-            })
-          }
-        })
-      }
-    )
-  // }
-
-
+  }
 }
 
-function slideDown(el, dur, step, callback) {
-  let height = el.offsetHeight
-  let counter = height;
-  el.style.height = "auto"
-  // let interval = setInterval(
-  //   function () {
-  //     counter += step;
-  //     if (counter < height) {
-  //       el.style.height = counter + "px";
-  //     } else {
-  //       el.style.height = height + "px";
-  //       clearInterval(interval);
-  //       if (callback) {
-  //         callback()
-  //       }
-  //     }
-  //   }, dur);
-}
+function slideDown(el, interval, callback) {
 
-function slideUp(el, dur, step, callback) {
+  let siblings = el.children
+  let exactSiblings = Array.from(siblings).reduce(
+    (acc, el, index) => {
+      return el.tagName === 'LI' ? acc + el.scrollHeight : null
+    }, 0
+  )
 
-  let height = el.offsetHeight
-  let counter = height;
-
-  let interval = setInterval(
+  let height = exactSiblings
+  let counter = 0;
+  let step = height / 30
+  let int = setInterval(
     function () {
-      counter -= step;
-      if (counter > 0) {
+      counter += step;
+      if (counter < height) {
         el.style.height = counter + "px";
       } else {
-        el.style.height = 0;
-        clearInterval(interval);
+        el.style.height = height + "px";
+        clearInterval(int);
         if (callback) {
           callback()
         }
       }
-    }, dur);
+    }, interval);
 }
 
+function slideUp(el, interval, callback) {
 
+  let parent = el.parentNode.parentNode
+  let height = el.offsetHeight
+  let heightToDistractFromParent = height
+  let step = height / 30
+  let counter = height;
+  let pCounter = parent.offsetHeight;
+  let pInitHeight= parent.offsetHeight;
 
-//
+  let int = setInterval(
+    function () {
+      el.style.display = `block`
+      counter -= step;
+      pCounter -= step
+      if (counter > 0) {
+        el.style.height = counter + "px";
+        parent.style.height = pCounter + "px"
+      } else {
+        el.style.height = 0;
+        clearInterval(int);
+        parent.style.height = pInitHeight
+        if (callback) {
+          callback()
+        }
+      }
+    }, interval, heightToDistractFromParent);
+}
+
+function closeSiblings(parent) {
+  Array.from(parent.children).map((el) => {
+      let elArr = Array.from(el.children)
+      elArr.map((ch) => {
+        if (ch.type && ch.type === 'checkbox' && ch.checked) {
+          let ul = ch.nextSibling.nextSibling
+          slideUp(ul, 0, function () {
+            ch.checked = false
+          })
+        }
+      })
+    }
+  )
+}
+
 // var adder = height / 10; //the height is global variable
 //
 // //iteratively increase the height
@@ -85,13 +103,9 @@ function slideUp(el, dur, step, callback) {
 //     }
 // }, duration);
 
-
-
 // //#3 the slideDown
 // function slideDown(a, b) {
-//
 //     var adder = height / 10; //the height is global variable
-//
 //     //iteratively increase the height
 //     interval = setInterval(function () {
 //         counter += adder;
@@ -106,7 +120,6 @@ function slideUp(el, dur, step, callback) {
 //             clearInterval(interval);
 //         }
 //     }, duration);
-//
 // }
 
 
