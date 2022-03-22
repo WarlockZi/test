3606d3808c1d278c3de737a05ad63c994039cf61
Vitaliy <vvoronik@yandex.ru>
2022-03-22 18:35:03 +0300

    debuonce

--- public/src/components/contenteditable.js
+++ public/src/components/contenteditable.js
@@ -9,6 +9,32 @@ export default function contenteditable() {
     $(model).on('keyup', handle.bind(model, contenteditable))
   }
 
+
+
+
+
+
+  function handleInput(e) {
+  }
+  searchInput.addEventListener("input", handleInput);
+
+  function debounce(callee, timeoutMs) {
+    return function perform(...args) {
+      let previousCall = this.lastCall;
+      this.lastCall = Date.now();
+      if (previousCall && this.lastCall - previousCall <= timeoutMs) {
+        clearTimeout(this.lastCallTimer);
+      }
+      this.lastCallTimer = setTimeout(() => callee(...args), timeoutMs);
+    };
+  }
+
+
+
+
+
+
+
   function handle(e) {
     if (['NumpadEnter', 'Enter'].includes(e.code)) {
       save()
