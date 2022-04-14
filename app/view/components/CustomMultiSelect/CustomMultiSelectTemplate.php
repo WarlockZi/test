92b80b394727d64faef506d3f81e44584721257b
Vitaliy <vvoronik@yandex.ru>
2022-04-14 18:24:25 +0300

    multiselect.js arrowDromDownIcon chip wdss

--- app/view/components/CustomMultiSelect/CustomMultiSelectTemplate.php
+++ app/view/components/CustomMultiSelect/CustomMultiSelectTemplate.php
@@ -1,28 +1,57 @@
-<select
-		multiple="true"
-<!--		custom-select-->
-		data-field="<?= $model->field; ?>"
-	<?= $model->title ? "title='{$model->title}'" : ''; ?>
-	<?= $model->className ? "class={$model->className}" : ''; ?>
+<div class="multiselect" tabindex="0">
+	<div class="wrap">
+		<div class="chip-wrap">
+				<? foreach ($model->tree as $k => $v): ?>
 
->
+			  <div class="chip" data-id="<?= $v['id']; ?>"><?= $v[$model->fieldName] ?>
+				  <div class="del">Х</div>
+			  </div>
 
-	<? foreach ($model->tree as $k => $v): ?>
+				<? endforeach; ?>
+		</div>
 
-	  <option
-			  value="<?= $v['id'] ?>"
-			  <?=in_array($v['id'],$model->selected)?'selected':'';?>
-	  >
-			 <?= $v[$model->fieldName] ?>
-	  </option>
-		<? $level = 0; ?>
-		<? if (isset($v['childs'])): ?>
-			<?= $model->getChilds($v['childs'], $level); ?>
-		<? endif ?>
+		<div class="arrow">
+				<? include ICONS . '/ArrowDropDownIcon.svg' ?>
+		</div>
 
-	<? endforeach; ?>
+		<select
+				multiple="true"
+				custom-select
+				data-field="<?= $model->field; ?>"
+				<?= $model->title ? "title='{$model->title}'" : ''; ?>
+				<?= $model->className ? "class={$model->className}" : ''; ?>
+
+		>
+
+				<? foreach ($model->tree as $k => $v): ?>
+
+			  <option
+					  value="<?= $v['id'] ?>"
+						 <?= in_array($v['id'], $model->selected) ? 'selected' : ''; ?>
+			  >
+						 <?= $v[$model->fieldName] ?>
+			  </option>
+					<? $level = 0; ?>
+					<? if (isset($v['childs'])): ?>
+						<?= $model->getChilds($v['childs'], $level); ?>
+					<? endif ?>
+
+				<? endforeach; ?>
 
 
-</select>
+		</select>
+
+	</div>
+
+	<ul>
+		<li class="inner">
+				<? foreach ($model->tree as $k => $v): ?>
+			  <label for="<?= $v[$model->fieldName] ?>"
+			         data-id="<?= $v['id']; ?>"
+			  ><?= $v[$model->fieldName] ?></label>
+				<? endforeach; ?>
+		</li>
+	</ul>
 
 
+</div>
--- public/src/components/multiselect/multiselect.js
+++ public/src/components/multiselect/multiselect.js
@@ -1,2 +1,63 @@
 import './multiselect.scss'
+import {$} from '../../common'
 
+export default function multiselect() {
+
+  let PHPmultiselect = $('.multiselect')
+  if (PHPmultiselect) {
+
+    [].forEach.call(PHPmultiselect, function (select) {
+
+      select.addEventListener('click', handleClick, false)
+      select.addEventListener('blur', handleBlur, false)
+
+      function handleBlur({target}) {
+        let show = $(this).find('.show')
+        if (show) {
+          show.classList.remove('show')
+        }
+      }
+
+      function handleClick({target}) {
+
+        if (target.closest('.wrap') && target.tagName.toLowerCase() === 'svg') {
+          let multiselect = target.closest('.multiselect')
+          let ul = multiselect.querySelector('ul')
+          ul.classList.toggle('show')
+
+        } else if (['del'].includes(target.className)) {
+          let chip = target.closest('.chip')
+          chip.remove()
+
+        } else if (target.tagName.toLowerCase() === 'label') {
+          let id = target.dataset.id;
+          let m = target.closest('.multiselect')
+          let chips = m.querySelectorAll('.chip');
+          let exist = [].some.call(chips, (chip) => {
+            return chip.dataset.id === id
+          })
+          if (!exist) {
+            let wrap = $(m).find('.chip-wrap')
+            let chip = createChip(id)
+            wrap.append(chip)
+          }
+        }
+
+        function createChip(id) {
+          let chip = document.createElement('div')
+          chip.classList.add('chip')
+          chip.innerText = target.innerText
+          chip.dataset['id'] = id
+
+          let del = document.createElement('div')
+          del.classList.add('del')
+          del.innerText = 'X'
+
+          chip.append(del)
+
+          return chip
+        }
+      }
+    })
+  }
+}
