71ee41d449613049cc46bd374d53415684e7ce84
Vitaliy <vvoronik@yandex.ru>
2022-05-14 13:51:52 +0300

    accordion fix not finished

--- public/src/components/accordion/accordion.js
+++ public/src/components/accordion/accordion.js
@@ -11,28 +11,26 @@ if (accordions) {
     })
   }
 
-  [].forEach.call(accordions, function (acc) {
-  // debugger
-    $(acc).on('click', handleToggle)
-  })
+  $('.admin-layout__sidebar').on('click', handleCl)
+  // $('[accordion]').on('click', handleCl)
 }
 
 
-function handleToggle({target}) {
-
+function handleCl({target}) {
+// alert('dd')
   let accordion = target.closest('[accordion]')
   let li = target.closest('li')
   let checkbox = $(li).find(`[type='checkbox']`)
   let ul = $(li).find('ul')
 
-
-  if (checkbox.checked) {
+  if (checkbox.checked&&ul) {
     slideUp(ul, 0,)
   } else {
+    if (!ul)return
     accordion.style.height = "auto"
     slideDown(ul)
-    let ulHeight = ul.scrollHeight
-    increaseParent(accordion, ulHeight)
+    // let ulHeight = ul.scrollHeight
+    // increaseParent(accordion, ulHeight)
     // debugger
     closeSiblings(accordion)
   }
@@ -45,18 +43,10 @@ function increaseParent(parent, ulHeight) {
   }
 }
 
-
-function slideDown(ul, callback) {
-  ul.style.maxHeight = ul.scrollHeight + "px";
-  if (callback) {
-    callback()
-  }
-}
-
 function closeSiblings(parent) {
   Array.from(parent.children).map((el) => {
-      let elArr = Array.from(el.children)
-      elArr.map((ch) => {
+
+      [].map.call(el.children,(ch) => {
         if (ch.type && ch.type === 'checkbox' && ch.checked) {
           let ul = $(ch.parentNode).find('ul')
           slideUp(ul, 0, function () {
@@ -66,6 +56,13 @@ function closeSiblings(parent) {
       })
     }
   )
+}
+
+function slideDown(ul, callback) {
+  ul.style.maxHeight = ul.scrollHeight + "px";
+  if (callback) {
+    callback()
+  }
 }
 
 function slideUp(ul, interval, callback) {
