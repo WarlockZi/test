3586d0fdf557b9351d07e401c086ddf4d348b04f
Vitaliy <vvoronik@yandex.ru>
2022-05-30 17:08:08 +0300

    question create

--- app/controller/OpenanswerController.php
+++ app/controller/OpenanswerController.php
@@ -16,26 +16,14 @@ class OpenanswerController Extends AppController
 		$this->autorize();
 	}
 
-	public function actionCreate()
-	{
-		$q_id = $this->ajax['q_id'];
-		$answerCnt = $this->ajax['answerCnt'];
-
-		$index = Openanswer::create(['openquestion_id' => $q_id]) - 1;
-		$i = $answerCnt + 1;
-
-		ob_start();
-		include ROOT . '/app/view/Opentest/edit_BlockAnswer.php';
-		$this->exitWith(ob_get_clean());
-
-	}
-
 	public function actionUpdateOrCreate()
 	{
 		try {
 			if ($this->ajax) {
 				$res = $this->model::updateOrCreate($this->ajax);
 				if (is_int($res)) {
+					$a['id'] = $res;
+					$a['answer'] = '';
 					$i = $this->ajax['sort'];
 
 					ob_start();
@@ -50,16 +38,6 @@ class OpenanswerController Extends AppController
 		};
 	}
 
-
-//	public function actionUpdate()
-//	{
-//		$this->model::updateOrCreate($this->req['question']);
-//
-//		foreach ($this->req['answers'] as $answer) {
-//			Openanswer::updateOrCreate($answer);
-//		}
-//		exit(json_encode(['msg' => 'Saved']));
-//	}
 
 	public function actionDelete()
 	{
