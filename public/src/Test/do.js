2ea33699d5515995143e71a59fd75d11994edc21
Vitaliy <vvoronik@yandex.ru>
2022-06-27 14:24:37 +0300

    dix

--- public/src/Test/do.js
+++ public/src/Test/do.js
@@ -3,36 +3,29 @@ import './test-pagination/test-pagination'
 
 import {post, $, cachePage} from '../common'
 
-let testDo = $('.test-do')
+let testDo = $('.test-do')[0]
 if (testDo) {
   showFirstQuest()
   finishBtnInit()
-  $('.test-do').on('click', handleClick)
-}
-
-function finishBtnInit() {
-  let button = $('.test-do__finish-btn')[0]
-  if (button) {
-    if (window.location.pathname.match('^/test/result/.?')) {
-      button.classList.add('inactive')
-    }
-  }
+  testDo.addEventListener('click', handleClick)
 }
 
 function handleClick({target}) {
   if (target.classList.contains('accordion-open')) return
-  let currQuest = $('.question.show')[0]??''
-  let id = +currQuest?.dataset.id??''
-  let navs = $('[data-pagination]')??''
-  let navIndex = navs.findIndex(el=>el.classList.contains('active'))??''
+  let currQuest = $('.question.show')[0] ?? ''
+  let id = +currQuest?.dataset.id ?? ''
+  let navs = $('[data-pagination]') ?? ''
+  let navIndex = navs.findIndex(el => el.classList.contains('active')) ?? ''
 
   if (target.type === "checkbox") {
-    let a = target.labels[0]
-    a.classList.toggle('pushed')
+    let answer = target.labels[0]
+    answer.classList.toggle('pushed')
   } else if (target.id === 'prev') {
     prevQ()
   } else if (target.id === 'next') {
     nextQ()
+  } else if (target.classList.contains('test-do__finish-btn')) {
+    finishTest(target)
   }
 
   function prevQ() {
@@ -43,7 +36,7 @@ function handleClick({target}) {
   }
 
   function nextQ() {
-    if (navIndex === navs.length-1) return false
+    if (navIndex === navs.length - 1) return false
     let aimId = +navs[navIndex + 1].dataset.pagination
 
     pushNav(id, aimId)
@@ -65,36 +58,26 @@ function handleClick({target}) {
   }
 }
 
-function showFirstQuest() {
-  $('.question').removeClass("show")
-  $('.question:first-child').addClass("show")
-}
+async function finishTest(target) {
 
-$('.test-do__finish-btn').on('click', async function ({target}) {
-
-  let button = target;
-  if (button.id !== 'btnn') return false
-
-  if (button.text === "ПРОЙТИ ТЕСТ ЗАНОВО") {
+  if (target.innerText === "ПРОЙТИ ТЕСТ ЗАНОВО") {
     location.reload();
     return;
   }
-  button.text = "ПРОЙТИ ТЕСТ ЗАНОВО"
-
-  button.classList.add('inactive')
+  target.innerText = "ПРОЙТИ ТЕСТ ЗАНОВО"
+  target.classList.add('inactive')
 
   let corrAnswers = await post('/test/getCorrectAnswers', {})
-  corrAnswers = corrAnswers['arr']
-  let errorCnt = colorView(corrAnswers)
+  colorView(corrAnswers.arr)
+  let errorCnt = $('.redShadow').length
 
   let data = objToServer(errorCnt)
   let res = await post('/adminsc/testresult/create', data)
   if (res) {
-    $("#btnn")[0].href = location.href
-    $("#btnn")[0].text = "ПРОЙТИ ТЕСТ ЗАНОВО"
+    target.href = location.href
+    target.innerText = "ПРОЙТИ ТЕСТ ЗАНОВО"
   }
-})
-
+}
 
 function objToServer(errorCnt) {
   return {
@@ -109,41 +92,66 @@ function objToServer(errorCnt) {
 
 
 function colorView(correctAnswers) {
-  let q = $('.question');
-  [].map.call(q, function (question) {
-    let answers = question.querySelectorAll('.a')
-    let errors = [];
-    [].map.call(answers, function (answer) {
-      let input = $(answer).find('input')
-      let id = answer.dataset.id
-      checkCorrectAnswers(errors, id, correctAnswers, input, answer)
-    })
-
-    let id = question.dataset['id'] // id question
-    let paginItem = $(`.pagination [data-pagination='${+id}']`)[0]
-    if (errors.length) {
-      $(paginItem).addClass('redShadow')
-    } else {
-      $(paginItem).addClass('greenShadow')
-    }
+  let questions = $('.question');
+  [].map.call(questions, (question)=> {
+    let errors = colorQuestions(question, correctAnswers)
+    colorPgination(question, errors)
   })
-  return $('.redShadow').length
 }
 
-function checkCorrectAnswers(errors, id, correctAnswers, input, answer) {
+function colorPgination(question, errors) {
+  let id = question.dataset['id']
+  let paginItem = $(`.pagination [data-pagination='${+id}']`)[0]
+  if (errors.length) {
+    $(paginItem).addClass('redShadow')
+  } else {
+    $(paginItem).addClass('greenShadow')
+  }
+}
+
+function colorQuestions(question, correctAnswers) {
+  let answers = question.querySelectorAll('.a');
+  let errors = [];
+  [].map.call(answers, (answer)=> {
+    let input = $(answer).find('input')
+    let id = answer.dataset.id
+    let error = checkCorrectAnswers(id, correctAnswers, input, answer)
+    if (error)errors.push(true)
+  })
+  return errors
+}
+
+function checkCorrectAnswers(id, correctAnswers, input, answer) {
   let correctAnser = correctAnswers.indexOf(id) !== -1
   let checked = input.checked
+  let error = false
 
   if (checked && correctAnser) {// checkbox нажат. а в correct answer нету. в correct_answers есть, его всегда подсвечиваем зеленым
     answer.classList.add('done'); //green check зеленый значек
   } else if (checked && !correctAnser) {// checkbox нажат,и есть в correct answer. в correct_answers нет, кнопка не нажата
-    errors.push(true)
+    error = true
   } else if (!checked && correctAnser) {// кнопка не нажата, в correct_answers есть
     answer.classList.add('done'); //green check зеленый значек
-    errors.push(true)
+    error =true
   } else if (!checked && !correctAnser) {// кнопка не нажата, в correct_answers нет
   }
+  return error
 }
+
+function finishBtnInit() {
+  let button = $('.test-do__finish-btn')[0]
+  if (button) {
+    if (window.location.pathname.match('^/test/result/.?')) {
+      button.classList.add('inactive')
+    }
+  }
+}
+
+function showFirstQuest() {
+  $('.question').removeClass("show")
+  $('.question:first-child').addClass("show")
+}
+
 
 
 
