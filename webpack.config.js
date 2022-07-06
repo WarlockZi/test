e30f73ad3fd114c8ff410a0e8acf933fcc7d34fb
Vitaliy <vvoronik@yandex.ru>
2022-07-06 18:02:28 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -31,23 +31,37 @@ const config = {
   },
   output: {
     path: path.resolve(__dirname, "public/dist"),
+    filename:'[name].js',
     clean: true,
   },
   // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all'
-  //   },
   //   runtimeChunk: 'single',
+  //   splitChunks: {
+  //     // chunks: 'all',
+  //     // maxInitialRequests: Infinity,
+  //     // minSize: 0,
+  //     // cacheGroups: {
+  //     //   vendor: {
+  //     //     test: /[\\/]node_modules[\\/]/,
+  //     //     name(module) {
+  //     //       // get the name. E.g. node_modules/packageName/not/this/part.js
+  //     //       // or node_modules/packageName
+  //     //       const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+  //     //
+  //     //       // npm package names are URL-safe, but some servers don't like @ symbols
+  //     //       return `npm.${packageName.replace('@', '')}`;
+  //     //     },
+  //     //   },
+  //     // },
+  //   },
   // },
 
 
   devServer: {
-
     allowedHosts: "all",
     host: "localhost",
     port: 4000,
     hot: true,
-
     watchFiles: {
       paths: ['public/src/**/*.*'],
     },
