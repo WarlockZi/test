a5028281f9bf60c7995ec65a2aadba67ec405d75
Vitaliy <vvoronik@yandex.ru>
2022-11-22 11:35:44 +0300

    config rights fix

--- app/config/Config.php
+++ app/config/Config.php
@@ -7,9 +7,10 @@ namespace app\config;
 class Config
 {
   private static $configRights = [
+    'user_update'=>'возможность редактировать свой профиль',
     'role_admin'=>"возможность добавлять права другим пользователям, добавлять некоторые свойства и др",
     'role_employee'=>'возможность заходить в админку, проходить тесты, смотреть обучающие видео',
-    'test_delete'=>'возможность удалять тесты'
+    'test_delete'=>'возможность удалять тесты',
   ];
 
   public static function getConfigRights()
