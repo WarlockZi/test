55ee56a7a850265c4ccf0f82c29ccbb35ac6bc16
Vitaliy <vvoronik@yandex.ru>
2022-11-22 09:51:09 +0300

    fix user2

--- app/controller/AuthController.php
+++ app/controller/AuthController.php
@@ -238,7 +238,7 @@ class AuthController extends AppController
 	public static function user()
 	{
 		if (isset($_SESSION['id']) && $_SESSION['id']) {
-			$user = IlluminateUser::find($_SESSION['id'])->toArray();
+			$user = IlluminateUser::find($_SESSION['id']);
 			if (!$user) {
 				exit(json_encode(['popup' => 'Неправильные данные для входа на сайт']));
 			}
