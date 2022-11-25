b9169af80fa81268d9e6fce0f8290199e5b480a1
Vitaliy <vvoronik@yandex.ru>
2022-11-25 19:39:24 +0300

    image morph

---
+++ public/src/Admin/Image/Image.js
@@ -0,0 +1,12 @@
+import Morph from "../../components/morph/morph";
+
+export default class Image extends Morph {
+  constructor(props) {
+    super(props);
+    this.tag = props?.tag
+
+  }
+
+  tag
+
+}
