1fba9d49a7db4105f161e96a2c08d0e86114df73
Vitaliy <vvoronik@yandex.ru>
2023-04-04 09:44:42 +0300

    zoom fix

--- public/src/Product/card.js
+++ public/src/Product/card.js
@@ -5,36 +5,45 @@ import {$} from '../common'
 
 window.onload = function () {
 
-  let img = $('.zoom').first()
-  img.onmousemove = function (e) {
-    let offsetX = 0
-    let offsetY = 0
-    var zoomer = e.currentTarget;
+  let zoom = $('.zoom').first()
+  if (zoom) {
 
-    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
-    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
-    let x = offsetX / zoomer.offsetWidth * 100
-    let y = offsetY / zoomer.offsetHeight * 100
-    zoomer.style.backgroundPosition = x + '% ' + y + '%';
+    zoom.onmousemove = function (e) {
+      let offsetX = 0
+      let offsetY = 0
+      var zoomer = e.currentTarget;
+
+      e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+      e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+      let x = offsetX / zoomer.offsetWidth * 100
+      let y = offsetY / zoomer.offsetHeight * 100
+      zoomer.style.backgroundPosition = x + '% ' + y + '%';
+    }
   }
 
 
   let quillSelector = '.detail-text'
+  let textarea =  $(quillSelector)[0]
 
-  let innertext = $(quillSelector)[0].innerText
+  if (textarea){
+  let innertext =textarea.innerText
+    if (isJsonString(innertext)) {
+      let text = JSON.parse(textarea.innerText)
+      var options = {
+        placeholder: 'Compose an epic...',
+        // theme: 'bubble'
+        // theme: 'snow'
+      };
 
-  if (isJsonString(innertext)) {
-    let text = JSON.parse(textarea.innerText)
-    var options = {
-      placeholder: 'Compose an epic...',
-      // theme: 'bubble'
-      // theme: 'snow'
-    };
-
-    var q = new Quill(quillSelector, options);
-    q.setContents(text)
-    q.enable(false)
+      var q = new Quill(quillSelector, options);
+      q.setContents(text)
+      q.enable(false)
+    }
   }
+
+
+
+
 
 
   function isJsonString(str) {
