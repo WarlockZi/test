394ddec6422cd75ce433675a5ec564378810a609
Vitaliy <vvoronik@yandex.ru>
2023-04-07 17:07:41 +0300

    indegration

--- app/controller/IntegrationController.php
+++ app/controller/IntegrationController.php
@@ -16,6 +16,19 @@ class IntegrationController extends AppController
 
 	public function actionIndex()
 	{
+
+		$text = '';
+		if (isset($_POST)){
+			$text .= json_encode($_POST);
+		}
+		if (isset($_FILES)){
+			$text .= json_encode($_FILES);
+		}
+		if (isset($_GET)){
+			$text .= json_encode($_GET);
+		}
+		$path = ROOT.'/pic/integration.txt';
+		file_put_contents($path,$text);
 		echo "success\ncatalog\ncheckauth";
 //		header('Content-Type',null, 200);
 	}
