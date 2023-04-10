71641e50d948ae69d1ce4f6b8d5f81efd5864cdb
Vitaliy <vvoronik@yandex.ru>
2023-04-10 09:15:24 +0300

    integration

--- app/controller/XmlController.php
+++ app/controller/XmlController.php
@@ -16,26 +16,27 @@ class XmlController extends AppController
 	{
 
 		$text = '';
-		if (isset($_POST)){
+		if (isset($_POST)) {
 			$text .= json_encode($_POST);
 		}
-		if (isset($_FILES)){
+		if (isset($_FILES)) {
 			$text .= json_encode($_FILES);
 		}
-		if (isset($_GET)){
+		if (isset($_GET)) {
 			$text .= json_encode($_GET);
 		}
-		$path = ROOT.'/pic/integration.txt';
-		file_put_contents($path,$text);
+		$path = ROOT . '/pic/integration.txt';
+		file_put_contents($path, $text, FILE_APPEND);
 //		echo "success\ncatalog\ncheckauth";
+		echo "success";
 //		header('Content-Type',null, 200);
 	}
-		public function action1c_exchange()
+
+	public function action1s_exchange()
 	{
 		echo "success\n1s\nintegr";
 //		header('Content-Type',null, 200);
 	}
-
 
 
 }
