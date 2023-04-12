e76fcff7a1d77e7f4e84fafa60d0b80ca4bb4b57
Vitaliy <vvoronik@yandex.ru>
2023-04-12 12:35:15 +0300

    set coockie

--- app/view/Header/admin/admin_menu__accordion.php
+++ app/view/Header/admin/admin_menu__accordion.php
@@ -194,6 +194,7 @@ use \app\core\Icon;
 				  <a class="neon" href='/adminsc/settings/cache'>Очистить кэш</a>
 				  <a class="neon" href="/adminsc/xml">Загрузить товары из файла</a>
 				  <a class="neon" href="/adminsc/xml/incread">Файл ответа</a>
+				  <a class="neon" href="/adminsc/xml/incclear">Очисть файл ответа</a>
 			  </ul>
 		  </li>
 		 <? endif; ?>
