d7de4e96e81acaedc8aeea8c17ceb1e11d06d971
Vitaliy <vvoronik@yandex.ru>
2023-04-14 15:46:43 +0300

    user f

--- app/view/Header/phone.php
+++ app/view/Header/phone.php
@@ -3,7 +3,7 @@
 		8 (909) 594-29-11</a>
 	<div class="wrap">
 		<div class="inner">
-			<div class="head">Время работы 8:30 – 17.30 по Москве</div>
+			<div class="head">Время работы 8:30 – 17.00 по Москве</div>
 			<div>Дополнительные телефоны:</div>
 			<div class="phones">
 				<a href="tel:+79095942911">8 (921) 713-17-67</a>
