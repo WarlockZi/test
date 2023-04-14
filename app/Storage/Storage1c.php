f8e9541ef4601fd3b0ca499e1400b83d8efbe422
Vitaliy <vvoronik@yandex.ru>
2023-04-14 10:38:50 +0300

    upload storage

--- app/Storage/Storage1c.php
+++ app/Storage/Storage1c.php
@@ -11,7 +11,7 @@ class Storage1c extends Storage
 	public function __construct()
 	{
 		parent::__construct();
-		$this->path = $this->storagePath.'1c_uploads/';
+		$this->path = $this->storagePath.'uploads'.DIRECTORY_SEPARATOR;
 	}
 
 	public static function getPath(){
