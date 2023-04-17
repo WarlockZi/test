56753ce3f11b250cf242df0a7ab00f845f6ca0af
Vitaliy <vvoronik@yandex.ru>
2023-04-17 11:57:37 +0300

    meta

--- app/view/Assets/Assets.php
+++ app/view/Assets/Assets.php
@@ -35,6 +35,7 @@ class Assets
 
 	public function setItemMeta(Model $item)
 	{
+		echo "title {$item->title} name {$item->name} de {$item->description} ke {$item->keywords}";
 		$this->title = $item->title ?? $item->name;
 		$this->desc = $item->description ? $item->description : $item->name;
 		$this->keywords = $item->keywords ? $item->keywords : $item->name;
