b67f1745c1f09653e87130d27d4ac412ea7a5bc6
Vitaliy <vvoronik@yandex.ru>
2023-04-20 10:02:58 +0300

    popup

--- app/controller/Admin/OrderController.php
+++ app/controller/Admin/OrderController.php
@@ -10,10 +10,6 @@ use app\view\Order\OrderView;
 
 class OrderController Extends AppController
 {
-//	public $modelName = 'order';
-//	public $model = Order::class;
-//	public $table = 'orders';
-
 	public function __construct()
 	{
 		parent::__construct();
--- public/src/Main/main.js
+++ public/src/Main/main.js
@@ -1,9 +1,10 @@
 // import '../Auth/auth'
 import './main.scss'
+import popup from '../popup/popup'
 
 import '../components/header/autocomplete'
 // import '../Product/card'
 import '../Category/category'
 
-
+popup();
 
