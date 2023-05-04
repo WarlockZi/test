6d9d3d9f569804b57b3858be1e48a3ffaef2cbf1
Vitaliy <vvoronik@yandex.ru>
2023-05-04 09:09:18 +0300

    cart

--- app/view/Product/product.php
+++ app/view/Product/product.php
@@ -30,26 +30,26 @@ if ($product): ?>
 			</div>
 
 
-			<div class="to-cart">
-					 <?= ProductView::renderToCart($product); ?>
-
-				<div class="adjust none">
-					<a href="/cart" class="button green">
-						<span class='bigger'>В корзину</span>
-					</a>
-
-					<div class="plus-minus">
-						<button tabindex="0" class="minus">
-							<?= Icon::minus()?>
-						</button>
-						<span class="digit" contenteditable="true">1</span>
-						<button tabindex="0" class="plus">
-							<?= Icon::plus1()?>
-
-						</button>
-					</div>
-				</div>
-			</div>
+<!--			<div class="to-cart">-->
+<!--					 --><?//= ProductView::renderToCart($product); ?>
+<!---->
+<!--				<div class="adjust none">-->
+<!--					<a href="/cart" class="button green">-->
+<!--						<span class='bigger'>В корзину</span>-->
+<!--					</a>-->
+<!---->
+<!--					<div class="plus-minus">-->
+<!--						<button tabindex="0" class="minus">-->
+<!--							--><?//= Icon::minus()?>
+<!--						</button>-->
+<!--						<span class="digit" contenteditable="true">1</span>-->
+<!--						<button tabindex="0" class="plus">-->
+<!--							--><?//= Icon::plus1()?>
+<!---->
+<!--						</button>-->
+<!--					</div>-->
+<!--				</div>-->
+<!--			</div>-->
 
 		</div>
 
