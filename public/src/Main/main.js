2d7cffdff481ec4de46f66c9af9d70ae1b6673ec
Vitaliy <vvoronik@yandex.ru>
2023-05-15 19:26:03 +0300

    form

--- public/src/Main/main.js
+++ public/src/Main/main.js
@@ -1,15 +1,13 @@
 import '../404/404.scss'
 import './main.scss'
-import popup from '../components/popup/popup'
+
 import cart from '../Cart/cart'
 
 import '../components/header/autocomplete'
-// import '../Product/card'
+
 import '../Category/category'
 
 document.addEventListener('DOMContentLoaded',function () {
-  // debugger
-  // popup();
   new cart()
 });
 
