eb8d321131f5a26d586869891f2efa69f47346f9
Vitaliy <vvoronik@yandex.ru>
2023-06-06 15:32:30 +0300

    to array

--- app/view/Product/card/priceUnit.php
+++ app/view/Product/card/priceUnit.php
@@ -2,7 +2,7 @@
 
 	<div class="price-for-unit">
 		<?=var_dump($price)?>
-		<?=var_dump($unit->pivot)?>
+		<?=var_dump($unit->pivot->toArray())?>
 		 <? $unit_price = $unit->pivot->multiplier * (float)$price; ?>
 		 <?= number_format($unit_price, 2, '.', ' ') ?>
 	</div>
