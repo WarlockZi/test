7bc6f02c74dd1f16ff5a6d18589765f21d0b1d87
Vitaliy <vvoronik@yandex.ru>
2023-06-11 20:39:42 +0300

    properties

--- public/src/Admin/Product/properties.js
+++ public/src/Admin/Product/properties.js
@@ -6,5 +6,21 @@ item.addEventListener('customSelect.changed', selectChanged);
 
 async function selectChanged(obj) {
   debugger;
-  let res = await post('/adminsc/product/changeval',obj)
+  let propertable_id = $('.item-wrap')[0].dataset.id;
+  let property_id = mutation.target.dataset.modelId;
+  let val_id = mutation.target.dataset.value;
+  post('/adminsc/product/setProperty',
+    {propertable_id, property_id, val_id});
+
+  let res = await post('/adminsc/product/changeval', obj)
+
 }
+// [].map.call(property, function (prop) {
+//   let observer = new MutationObserver(function (mutations) {
+//     mutations.forEach(function (mutation) {
+//     });
+//   });
+//   observer.observe(
+//     prop, {attributes: true,}
+//   );
+// })
