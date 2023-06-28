45b0d2d26a97326a0a239e462ce3a2619e461897
Vitaliy <vvoronik@yandex.ru>
2023-06-28 10:34:14 +0300

    stop

--- app/Services/XMLParser/LoadProducts.php
+++ app/Services/XMLParser/LoadProducts.php
@@ -20,7 +20,7 @@ class LoadProducts extends Parser
 		$this->goods = $this->xmlObj['Каталог']['Товары']['Товар'];
 		if ($this->logger)
 			$this->logger->write('--- products start ---'.$this->now());
-		exit('start');
+
 		$this->run();
 		if ($this->logger)
 			$this->logger->write('--- products stop ---'.$this->now());
