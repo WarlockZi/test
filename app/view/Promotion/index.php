fa401a7d841adc8e7a3d91d57ea069111bfdd40a
Vitaliy <vvoronik@yandex.ru>
2023-07-14 15:24:22 +0300

    promotions unit

--- app/view/Promotion/index.php
+++ app/view/Promotion/index.php
@@ -1,12 +1,16 @@
 <div class="promotions-index">
+	<div class="page-title">
+		Акции
+
+	</div>
 
 	<? foreach ($promotions as $promotion): ?>
 		<? if ($promotion->product): ?>
 		  <div class="promotion row">
 			  <div class="id">
 						 <?= $promotion->id;
-									 $unit = $promotion->product->baseUnit->name
-									 ?>
+						 $unit = $promotion->product->baseUnit->name
+						 ?>
 			  </div>
 
 			  <div class="old-price">
@@ -14,19 +18,19 @@
 			  </div>
 			  <div class="new-price">
 				  <div class="price">
-				  цена по акции - <?= $promotion->new_price;?>
+					  цена по акции - <?= $promotion->new_price; ?>
 
 				  </div>
 				  <div class="count">
-					  oт <?=$promotion->count;?> <?=$unit;?>
+					  oт <?= $promotion->count; ?> <?= $unit; ?>
 				  </div>
 			  </div>
-			  <a href="/product/<?=$promotion->product->slug?>" class="main-image">
+			  <a href="/product/<?= $promotion->product->slug ?>" class="main-image">
 				  <img src="<?= $promotion->product->mainImagePath; ?>" alt="">
 			  </a>
 
 			  <div class="active-till">
-				  действует до - <?=$promotion->active_till?>
+				  действует до - <?= $promotion->active_till ?>
 			  </div>
 		  </div>
 		<? endif; ?>
