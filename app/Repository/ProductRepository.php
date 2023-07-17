6447707e5f04b6b80a41e335b21c51ad56669750
Vitaliy <vvoronik@yandex.ru>
2023-07-17 11:04:14 +0300

    have no main unit report instore added

--- app/Repository/ProductRepository.php
+++ app/Repository/ProductRepository.php
@@ -123,7 +123,7 @@ class ProductRepository extends Controller
 //			//						->get(['id', 'art', 'name'])
 //		;
 
-		$all = DB::select("select * from `products` where not exists (select * from `unitables` as `b` where `b`.`product_id` = `products`.`1s_id`)");
+		$all = DB::select("select * from `products` where instore > 0 and not exists (select * from `unitables` as `b` where `b`.`product_id` = `products`.`1s_id`)");
 //		$all = DB::select("select * from `products` where exists (select * from `units` where `products`.`base_unit` = `units`.`id` and not exists (select * from `units` as `u` inner join `unitables` as `b` on `u`.`id` = `b`.`unitable_id` where `units`.`id` = `b`.`unit_id` and `b`.`unitable_type` = 'app\model\Unit' and `b`.`product_id` = `products`.`1s_id`))");
 
 //		$all = new Collection($all);
