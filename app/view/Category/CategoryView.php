064ed60c161508dcfd013f803d9d56753a7e5a49
Vitaliy <vvoronik@yandex.ru>
2023-11-28 20:04:58 +0300

    fix shop category products main image

--- app/view/Category/CategoryView.php
+++ app/view/Category/CategoryView.php
@@ -8,7 +8,6 @@ use app\model\Category;
 use app\model\Product;
 use app\Repository\ImageRepository;
 use app\view\Image\ImageView;
-use app\view\Product\ProductView;
 
 class CategoryView
 {
@@ -22,14 +21,8 @@ class CategoryView
 
 	public static function getProductMainImage(Product $product)
 	{
-		$file = ProductView::getMainImageFile($product);
-		if (is_readable($file)) {
 			$relativePath = ImageRepository::getProductMainImageSrc($product);
-			$srt = FS::getFileContent(__DIR__ . '/product_main_image.php', compact('relativePath', 'product'));
-			return $srt;
-		} else {
-			return ImageView::noImage();
-		}
+			return FS::getFileContent(__DIR__ . '/product_main_image.php', compact('relativePath', 'product'));
 	}
 
 	public static function getMainImage(Category $category)
@@ -40,21 +33,4 @@ class CategoryView
 			return ImageView::noImage();
 		}
 	}
-
-//	public static function selector(int $selected, int $exclude = -1): string
-//	{
-//		$cats = Category::all()->toArray();
-//		$parent_select = '<select>';
-//		$parent_select .= "<option value=0>---</option>";
-//		foreach ($cats as $t) {
-//			if ((int)$t['id'] !== $exclude) {
-//				$selectedStr = (int)$t['id'] === $selected ? 'selected' : '';
-//				$parent_select .= "<option value={$t['id']} {$selectedStr}>{$t['name']}</option>";
-//			}
-//		}
-//		$parent_select .= "</select>";
-//
-//		return $parent_select;
-//	}
-
 }
