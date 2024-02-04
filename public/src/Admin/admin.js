814551b4ad7082efdcee6bcdb87dff3e8579c6cb
Vitaliy <vvoronik@yandex.ru>
2024-02-04 21:34:57 +0300

    fix ci admin user/user

--- public/src/Admin/admin.js
+++ public/src/Admin/admin.js
@@ -30,6 +30,7 @@ import rights from './Rights/rights'
 import category from './Category/category1'
 // import pagination from './Product/pagination'
 import product from './Product/product'
+
 import user from './User/user'
 
 
