180b34e95f4e92d6139269aa66ef222638497d24
Vitaliy <vvoronik@yandex.ru>
2024-02-15 23:50:55 +0300

    fix github hook 1 and 2

--- app/controller/Admin/GithubController.php
+++ app/controller/Admin/GithubController.php
@@ -19,20 +19,12 @@ class GithubController Extends AppController
 		$logger = new FileLogger();
 		try {
 
-			$logger->write('webhook4');
-			$req = json_encode(count($_POST)). PHP_EOL ?? '1' . PHP_EOL;
-			$logger->write($req);
-
 			$logger->write('webhook1');
 			$req = serialize(file_get_contents('php://input') ). PHP_EOL ?? '1' . PHP_EOL;
 			$logger->write($req);
 
 			$logger->write('webhook2');
 			$req = json_encode(file_get_contents('php://input')). PHP_EOL ?? '2' . PHP_EOL;
-			$logger->write($req);
-
-			$logger->write('webhook3');
-			$req = json_decode(file_get_contents($_POST), true). PHP_EOL ?? '1' . PHP_EOL;
 			$logger->write($req);
 
 			http_response_code(200);
