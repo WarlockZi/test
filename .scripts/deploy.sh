4a0a0c7222f9441df7e84bd13507f1c7da78dedc
Vitaliy <vvoronik@yandex.ru>
2024-02-21 11:04:29 +0300

    dep sh

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -e
+
+# Загрузить последнюю версию приложения
+cd /var/www/vitexopt/data/www/vitexopt.ru
+git pull
+
+npm run build >> /var/www/vitexopt/data/www/vitexopt.ru/app/Storage/log/log.txt
+
+echo "Deployment finished!" >> app/Storage/log/log.txt
+#bash echo "Deployment finished!" >> /var/www/vitexopt/data/www/vitexopt.ru/app/Storage/log/log.txt
 
