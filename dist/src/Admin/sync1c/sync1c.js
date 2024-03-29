2dc07f753eb47666ba19ae9a7bf471410ac063a0
Vitaliy <vvoronik@yandex.ru>
2024-03-29 19:27:28 +0300

    add vite src delete dist and public dist

--- dist/src/Admin/sync1c/sync1c.js
+++
@@ -1,8 +0,0 @@
-import './sync1c.scss'
-import {Sync} from './Sync'
-import {$, popup, post} from '../../common'
-
-let $sync = $('.sync').first();
-if ($sync) {
-  new Sync($sync)
-}
--- dist/src/Auth/profile.js
+++
@@ -1,3 +0,0 @@
-import './profile.scss';
-
-
