0a1b82600c8d381c85dad3e89ac1d36312742dc5
Vitaliy <vvoronik@yandex.ru>
2024-04-11 13:58:32 +0300

    debug impload

--- app/Actions/XMLParser/LoadCategories.php
+++ app/Actions/XMLParser/LoadCategories.php
@@ -58,7 +58,7 @@ class LoadCategories extends Parser
 
 			$updated = Category::withTrashed()
 				->updateOrCreate(['1s_id' => $item['1s_id']], $item);
-			$this->logger->write('--- category  load ---' . $this->now(). $item.PHP_EOL);
+			$this->logger->write('--- category  load ---' . $this->now(). implode(',',$item). PHP_EOL);
 
 			return $item;
 		} catch (\Exception $e) {
