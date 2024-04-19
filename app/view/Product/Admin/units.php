45278d23a32bd753aba920abab421f46ced71430
Vitaliy <vvoronik@yandex.ru>
2024-04-19 09:03:57 +0300

    fix Admin/units

--- app/view/Product/Admin/units.php
+++ app/view/Product/Admin/units.php
@@ -6,40 +6,44 @@ use app\core\Icon;
 ?>
 <div class="units">
 
-	<div class="column">
-		<div class="title">Единицы</div>
+    <div class="column">
+        <div class="title">Единицы</div>
 
-		<div class="head">
-			<div class="name">Единица</div>
-			<div class="multiplier">Коэфф</div>
-			<div class="base-unit">Базовая ед.</div>
-			<div class="min-unit">Миним. отгруж ед.</div>
-			<div class="del"><?= Icon::trashIcon() ?></div>
-		</div>
+        <div class="head">
+            <div class="name">Единица</div>
+            <div class="multiplier">Коэфф</div>
+            <div class="base-unit">Базовая ед.</div>
+            <div class="min-unit">Миним. отгруж ед.</div>
+            <div class="del"><?= Icon::trashIcon() ?></div>
+        </div>
 
-		<div class="rows">
-			<div class="none">
-					 <?= $selector; ?>
-			</div>
-				<? foreach ($dopUnits as $unit): ?>
-			  <div class="row">
-						 <?= UnitFormView::selectorNew($baseUnit->id, $unit->id) ?>
-				  <input type="number" value="<?= $unit->pivot->multiplier ?>">
-				  <div class="base-unit"><?= $baseUnit->name ?></div>
+        <div class="rows">
+            <div class="none">
+                <?= $selector; ?>
+            </div>
+            <!--				--><? // foreach ($dopUnits as $unit): ?>
+            <? foreach ($baseUnit->units as $unit): ?>
+                <div class="row">
+                    <?= UnitFormView::selectorNew($baseUnit->id, $unit->id) ?>
 
-				  <div class="min-unit">
-					  <input type="checkbox" <?= $unit->pivot->is_main ? 'checked' : ''; ?>>
-				  </div>
-				  <div class="del">X</div>
-			  </div>
-				<? endforeach; ?>
-		</div>
+                    <!--                  --><?php //=UnitFormView::selectorNew($baseUnit->id, $unit->id) ?>
 
-		<div class="add-unit"
-		     data-unit="<?= $baseUnit->id ?>"
-		>+
-		</div>
-		 <?= $baseEqualsMainUnit; ?>
+                    <input type="number" value="<?= $unit->pivot->multiplier ?>">
+                    <div class="base-unit"><?= $baseUnit->name ?></div>
 
-	</div>
+                    <div class="min-unit">
+                        <input type="checkbox" <?= $unit->pivot->is_main ? 'checked' : ''; ?>>
+                    </div>
+                    <div class="del">X</div>
+                </div>
+            <? endforeach; ?>
+        </div>
+
+        <div class="add-unit"
+             data-unit="<?= $baseUnit->id ?>"
+        >+
+        </div>
+        <?= $baseEqualsMainUnit; ?>
+
+    </div>
 </div>
