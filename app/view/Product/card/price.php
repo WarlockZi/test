e88122027cd49b5d02a7cc6b4b58d7360328f378
Vitaliy <vvoronik@yandex.ru>
2024-04-19 09:28:26 +0300

    fix Product/price

--- app/view/Product/card/price.php
+++ app/view/Product/card/price.php
@@ -1,22 +1,26 @@
 <div class="price">
 
-	<?
-	$priceNumber = $product->getRelation('price')->price ?? 0;
-	$baseUnit = $product->baseUnit->name ?? '';
-	$formatedBasePrice = number_format($priceNumber, 2, '.', ' ');
-	$formatedOldPrice = number_format($priceNumber * 1.1, 2,);
-	?>
+    <?
+    $priceNumber       = $product->getRelation('price')->price ?? 0;
+    $baseUnit          = $product->baseUnit->name ?? '';
+    $formatedBasePrice = number_format($priceNumber, 2, '.', ' ');
+    $formatedOldPrice  = number_format($priceNumber * 1.1, 2,);
+    ?>
 
-	<div class="new-price"><?= $formatedBasePrice; ?> ₽ / <?= $baseUnit; ?> </div>
+    <div class="new-price"><?= $formatedBasePrice; ?> ₽ / <?= $baseUnit; ?> </div>
 
 </div>
 <div class="price-units ">
 
-	<? $arr = $product->baseUnit->units->toArray();
-
-    if (isset($product->dopUnits)): ?>
-		<? foreach ($product->dopUnits as $unit): ?>
-			<? include __DIR__ . '/priceUnit.php' ?>
-		<? endforeach; ?>
-	<? endif; ?>
+    <? $arr = $product->baseUnit->units->toArray();
+    if (isset($product->baseUnit->units)): ?>
+        <? foreach ($product->baseUnit->units as $unit): ?>
+            <? include __DIR__ . '/priceUnit.php' ?>
+        <? endforeach; ?>
+    <? endif; ?>
+    <!--    if (isset($product->dopUnits)): ?>-->
+    <!--		--><? // foreach ($product->dopUnits as $unit): ?>
+    <!--			--><? // include __DIR__ . '/priceUnit.php' ?>
+    <!--		--><? // endforeach; ?>
+    <!--	--><? // endif; ?>
 </div>
