088966bb848faff4f66456c3e941025303b61ade
Vitaliy <vvoronik@yandex.ru>
2024-05-03 12:39:22 +0300

    feat ErrorLogger.php

---
+++ app/Services/Logger/ErrorLogger.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace app\Services\Logger;
+
+
+use app\Storage\StorageLog;
+
+class ErrorLogger implements ILogger
+{
+	protected $logFile;
+
+	public function __construct($fileName = 'errors.txt')
+	{
+		$this->logFile = StorageLog::getFile($fileName);
+	}
+
+	public function read():string
+	{
+		return file_get_contents($this->logFile);
+	}
+
+
+	public function write(string $content):bool
+	{
+		return file_put_contents($this->logFile, $content.PHP_EOL, FILE_APPEND);
+	}
+
+	public function setFile(string $fileName): ILogger
+	{
+		$this->logFile = StorageLog::getFile($fileName);
+		return $this;
+	}
+
+	public function clear(): void
+	{
+		if ($this->logFile) file_put_contents($this->logFile,'');
+	}
+}
