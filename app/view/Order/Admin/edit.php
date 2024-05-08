ff52e43f066e82862e1f9f1c98bc977471e757b5
Vitaliy <vvoronik@yandex.ru>
2024-05-08 09:56:01 +0300

    fix order

--- app/view/Order/Admin/edit.php
+++ app/view/Order/Admin/edit.php
@@ -1,38 +1,48 @@
 <div class="order-edit">
 
-	<div class="page-name">Заказ <span id="order-id">&nbsp;новый</span></div>
+    <div class="page-name">Заказ <span id="order-id">&nbsp;новый</span></div>
 
-	<div class="row user">
-		<div class="client">
-			Клиент -
-		</div>
-		<div class="email"><?= $orders[0]->user->email ?></div>
-		<div class="fio"><?= $orders[0]->user->fi() ?></div>
-	</div>
+    <div class="row user">
+        <div class="client">
+            Клиент -
+        </div>
+        <div class="email"><?= $orders[0]->user->email ?></div>
+        <div class="fio"><?= $orders[0]->user->fi() ?></div>
+    </div>
 
-	<div class="row manager">
-		<div class="manager">
-			Менеджер -
-		</div>
-		 <?= \app\view\Product\ProductArrayFormView::getManagerSelector(); ?>
-	</div>
+    <div class="row manager">
+        <div class="manager">
+            Менеджер -
+        </div>
+        <?= \app\view\Product\ProductArrayFormView::getManagerSelector(); ?>
+    </div>
 
     <?php foreach ($orders as $order): ?>
         <?php if ($order->product): ?>
 
-		  <div class="row">
-			  <div class="num"><?= $order->id ?></div>
-			  <div class="name-price">
-				  <div class="name"><?= $order->product->name ?></div>
-				  <div class="price"></div>
-				  <!--		  <div class="id">--><?php //= $order->user->email ?><!--</div>-->
-			  </div>
-			  <div class="count"><?= $order->total_count ?></div>
-		  </div>
+            <div class="row">
+                <div class="num"><?= $order->id ?></div>
+                <div class="name-price">
+                    <div class="name"><?= $order->product->name ?></div>
+                    <div class="price"></div>
+                    <!--		  <div class="id">--><?php //= $order->user->email ?><!--</div>-->
+                </div>
+                <div class="count"><?= $order->total_count ?></div>
+                <?php if ($order->deleted_at): ?>
+                    <div class="active"><?= $order->deleted_at ?></div>
+                <?php else: ?>
+                    <div class="deleted"><?= $order->deleted_at ?></div>
+                <?php endif; ?>
+            </div>
         <?php else: ?>
-		  <div class="not-found">Товар не найден</div>
+            <div class="not-found">Товар не найден</div>
         <?php endif; ?>
+
     <?php endforeach; ?>
+    
+    <a href="/adminsc/order">
+        <div class="button">К списку заказов</div>
+    </a>
 
 </div>
 
