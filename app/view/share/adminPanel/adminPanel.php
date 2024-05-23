d2baa9119b75c5494cae11ab52ca9f30050fc05e
Vitaliy <vvoronik@yandex.ru>
2024-05-23 14:25:51 +0300

    fix file path

--- app/view/share/adminPanel/adminPanel.php
+++ app/view/share/adminPanel/adminPanel.php
@@ -1,13 +1,7 @@
 <? if (\app\core\Auth::isAdmin()): ?>
     <div class="admin-panel">
         <div class="download-sync">
-
-            <a href="/adminsc/sync/download">imp</a>
-<!--            <a href="/app/Storage/import/offers0_1.xml"-->
-<!--               target="_blank"-->
-<!--               download="offers0_1.xml"-->
-<!--            >off</a>-->
+            <a href="/adminsc/sync/download">download</a>
         </div>
-
     </div>
 <? endif; ?>
