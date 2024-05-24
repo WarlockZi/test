f794866813b0c11874279cdec61d77e728108bd6
Vitaliy <vvoronik@yandex.ru>
2024-05-24 14:26:48 +0300

    add errorLogger to sync conroller

--- app/Actions/SyncActions.php
+++ app/Actions/SyncActions.php
@@ -5,109 +5,120 @@ namespace app\Actions;
 
 
 use app\controller\AppController;
+use app\core\Error;
 use app\core\Route;
 use app\Repository\SyncRepository;
+use app\Services\Logger\ErrorLogger;
 use app\Storage\StorageDev;
 use app\Storage\StorageImport;
 
 class SyncActions extends AppController
 {
-	protected $importPath;
-	protected $storage;
-	protected $importFile;
-	protected $offerFile;
-	protected $viewPath = ROOT . '/app/view/Sync/Admin/';
-	protected $logger;
-	protected $repo;
-	public function __construct(SyncRepository $repo, $logger)
-	{
-		$this->setStorage();
-		$this->repo = $repo;
-		$this->logger = $logger;
+    protected $importPath;
+    protected $storage;
+    protected $importFile;
+    protected $offerFile;
+    protected $viewPath = ROOT . '/app/view/Sync/Admin/';
+    protected $logger;
+    protected ErrorLogger $errorLogger;
+    protected $repo;
 
-		$this->importFile = $this->storage::getFile('import0_1.xml');
-		$this->offerFile = $this->storage::getFile('offers0_1.xml');
-	}
+    public function __construct(SyncRepository $repo, $logger)
+    {
+        $this->setStorage();
+        $this->repo        = $repo;
+        $this->logger      = $logger;
+        $this->errorLogger = new ErrorLogger();
 
-	/**
-	 * @throws \Exception
-	 */
-	public function init()
-	{
-		if ($this->route->params['mode'] === 'checkauth') {
-			$this->checkauth();
-		} elseif ($this->route->params['mode'] === 'init') {
-			$this->zip();
-		} elseif ($this->route->params['mode'] === 'file') {
-			$this->file();
+        $this->importFile = $this->storage::getFile('import0_1.xml');
+        $this->offerFile  = $this->storage::getFile('offers0_1.xml');
+    }
 
-		} elseif ($this->route->params['mode'] === 'import') {
-			exit('success');
-		}
-	}
+    /**
+     * @throws \Exception
+     */
+    public function init()
+    {
+        try {
+            if ($this->route->params['mode'] === 'checkauth') {
+                $this->checkauth();
+            } elseif ($this->route->params['mode'] === 'init') {
+                $this->zip();
+            } elseif ($this->route->params['mode'] === 'file') {
+                $this->file();
 
-	private function importFilesExist(){
-		if (!is_readable($this->importFile)) {
-			$this->logger->write('Отсутстует файл importFile');
-			return false;
-		}
-		if (!is_readable($this->offerFile)) {
-			$this->logger->write('Отсутстует файл offerFile');
-			return false;
-		}
-		return true;
-	}
+            } elseif ($this->route->params['mode'] === 'import') {
+                exit('success');
+            }
+        } catch (\Throwable $exception) {
+            $this->errorLogger->write($exception->getMessage());
+        }
 
-	public function load()
-	{
-		try {
-			if (!$this->importFilesExist())exit('Отсутстуют импорт файлы');
+    }
 
-			$this->repo->softTrancate();
+    private function importFilesExist()
+    {
+        if (!is_readable($this->importFile)) {
+            $this->logger->write('Отсутстует файл importFile');
+            return false;
+        }
+        if (!is_readable($this->offerFile)) {
+            $this->logger->write('Отсутстует файл offerFile');
+            return false;
+        }
+        return true;
+    }
 
-			$this->repo->LoadCategories();
-			$this->repo->LoadProducts();
-			$this->repo->LoadPrices();
+    public function load()
+    {
+        try {
+            if (!$this->importFilesExist()) exit('Отсутстуют импорт файлы');
 
-		} catch (\Exception $e) {
-			exit(PHP_EOL . '---Ошибка загрузки: SyncActions---' . PHP_EOL . $e->getMessage() . PHP_EOL . $e);
-		}
-	}
+            $this->repo->softTrancate();
 
-	protected function checkauth()
-	{
-		$this->logReqest('checkauth');
-		exit("success\ninc\n777777\n55fdsa55");
-	}
+            $this->repo->LoadCategories();
+            $this->repo->LoadProducts();
+            $this->repo->LoadPrices();
 
-	protected function zip()
-	{
-		$this->logReqest('init');
-		exit("zip=no\nfile_limit=10000000");
-	}
+        } catch (\Exception $e) {
+            exit(PHP_EOL . '---Ошибка загрузки: SyncActions---' . PHP_EOL . $e->getMessage() . PHP_EOL . $e);
+        }
+    }
 
-	protected function file()
-	{
-		$filename = $this->route->params['filename'];
-		file_put_contents($this->importPath . $filename, file_get_contents('php://input'));
+    protected function checkauth()
+    {
+        $this->logReqest('checkauth');
+        exit("success\ninc\n777777\n55fdsa55");
+    }
 
-		$this->logReqest('file');
-		exit('success');
-	}
+    protected function zip()
+    {
+        $this->logReqest('init');
+        exit("zip=no\nfile_limit=10000000");
+    }
 
-	protected function logReqest($func)
-	{
-		$text = '<br>--' . date("H:i:s") . "--{$func}<br>";
-		if (isset($_GET)) {
-			$text .= '$_GET - ' . json_encode($_GET) . '<br>';
-		}
+    protected function file()
+    {
+        $filename = $this->route->params['filename'];
+        file_put_contents($this->importPath . $filename, file_get_contents('php://input'));
 
-		if (isset($this->route->params['filename'])) {
-			$text .= 'filename - ' . $filename = $this->route->params['filename'] . '<br>';
-			$text .= $this->importPath . $filename;
-		}
-		$this->logger->write($text);
-	}
+        $this->logReqest('file');
+        exit('success');
+    }
+
+    protected function logReqest($func)
+    {
+        $text = '<br>--' . date("H:i:s") . "--{$func}<br>";
+        if (isset($_GET)) {
+            $text .= '$_GET - ' . json_encode($_GET) . '<br>';
+        }
+
+        if (isset($this->route->params['filename'])) {
+            $text .= 'filename - ' . $filename = $this->route->params['filename'] . '<br>';
+            $text .= $this->importPath . $filename;
+        }
+        $this->logger->write($text);
+    }
 
     public function setStorage()
     {
