4b0214fb9b9ce8917da70fc5ba437eabfb20d1d9
Vitaliy <vvoronik@yandex.ru>
2024-05-29 12:13:17 +0300

    add contr

--- app/core/Cron/cron.php
+++ app/core/Cron/cron.php
@@ -1,5 +1,7 @@
 <?php
 require dirname(__DIR__,2).'/controller/Admin/SyncController.php';
+require dirname(__DIR__,2).'/controller/AppController.php';
+require dirname(__DIR__,2).'/controller/Controller.php';
 
 echo date("Y_m_d H:i:s") . " querry sent to sync/load" . PHP_EOL;
 
