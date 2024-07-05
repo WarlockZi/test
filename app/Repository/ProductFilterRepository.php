184c45398b84db0c8c692665469d60248439ccf5
Vitaliy new pc <vvoronik@yandex.ru>
2024-07-05 14:53:08 +0300

    fix olya

--- app/Repository/ProductFilterRepository.php
+++ app/Repository/ProductFilterRepository.php
@@ -4,20 +4,17 @@
 namespace app\Repository;
 
 use app\model\FilterUser;
-use app\model\Product;
-use Illuminate\Database\Capsule\Manager as DB;
-use Illuminate\Database\Eloquent\Builder;
 
 class ProductFilterRepository
 {
     public static function product(int $userId): array
     {
-        return FilterUser::where("user_id",$userId)
-            ->where('model','product')
+        $userFilters = FilterUser::where("user_id", $userId)
+            ->where('model', 'product')
             ->select('name')
-            ->first()
-            ->toArray()
-            ??[];
+            ->first();
+        return $userFilters ? $userFilters->toArray() : [];
+
     }
 
 }
