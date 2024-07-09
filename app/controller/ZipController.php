3ae2f1fb34d5815f752f99f1dc2fb5bbc8f11f76
Vitaliy new pc <vvoronik@yandex.ru>
2024-07-09 09:30:04 +0300

    fix debug files controller

--- app/controller/ZipController.php
+++ app/controller/ZipController.php
@@ -2,21 +2,28 @@
 
 namespace app\controller;
 
+use app\Services\Logger\ErrorLogger;
 use app\Services\ZipService\ImportFiles;
 use app\Services\ZipService\ZipService;
 
 class ZipController extends AppController
 {
     private ZipService $service;
+    private ErrorLogger $logger;
 
     public function __construct()
     {
         $this->service = new ZipService();
+        $this->logger  = new ErrorLogger();
     }
 
     public function actionDownload()
     {
         $files = (new ImportFiles)();
+        foreach ($files as $file) {
+            $this->logger->write($file);
+        }
+
         $this->service
             ->files($files)
             ->path('/app/Storage/import/')
