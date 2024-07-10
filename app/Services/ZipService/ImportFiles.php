e2df0b125093c2b47716d055ac508b872609d186
Vitaliy new pc <vvoronik@yandex.ru>
2024-07-10 12:43:30 +0300

    fix debug zip final

--- app/Services/ZipService/ImportFiles.php
+++ app/Services/ZipService/ImportFiles.php
@@ -2,20 +2,19 @@
 
 namespace app\Services\ZipService;
 
+use app\core\FS;
+
 class ImportFiles
 {
    public function __invoke():array
    {
       if ($_ENV['DEV'] == 1) {
-         return [
-            'import' => ROOT . '/app/Storage/dev/import0_1.xml',
-            'offer' => ROOT . '/app/Storage/dev/offers0_1.xml',
-         ];
+          $f  =1;
       } else {
-         return [
-            'import' => ROOT . '/app/Storage/import/import0_1.xml',
-            'offer' => ROOT . '/app/Storage/import/offers0_1.xml',
-         ];
       }
+         return [
+            'import' => FS::platformSlashes(ROOT . '/app/Storage/import/import0_1.xml'),
+            'offer' => FS::platformSlashes(ROOT . '/app/Storage/import/offers0_1.xml'),
+         ];
    }
 }
