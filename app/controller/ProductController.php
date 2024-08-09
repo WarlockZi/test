1dc338c550acb6a437fefb06e375e9fd9946290e
Vitaliy new pc <vvoronik@yandex.ru>
2024-08-09 11:06:56 +0300

    fix exception

--- app/controller/ProductController.php
+++ app/controller/ProductController.php
@@ -20,36 +20,35 @@ class ProductController extends AppController
 
     public function actionIndex(): void
     {
+        $slug = $this->route->slug;
+        if (!$slug) {
+            header('Location:/category');
+        }
+
         try {
-
-            $slug = $this->route->slug;
-            if (!$slug) {
-                header('Location:/category');
-            }
-
             $product = $this->repo->main($slug);
-            $this->route->setView('product');
-            if ($product) {
-                $oItems          = OrderRepository::count();
-                $breadcrumbs     = BreadcrumbsRepository::getCategoryBreadcrumbs($product->category_id, true,);
-                $userIsAdmin     = Auth::isAdmin();
-                $shippablePrices = $this->repo->dopUnitsPrices($product);
-
-                $this->set(compact('shippablePrices', 'product', 'breadcrumbs', 'oItems', 'userIsAdmin'));
-
-                $title    = $product->ownProperties->seo_title ?? $product->name;
-                $desc     = $product->ownProperties->seo_description ?? $product->name;
-                $keywords = $product->ownProperties->seo_keywords ?? $product->name;
-                $this->assets->setMeta($title, $desc, $keywords);
-
-                $this->assets->setProduct();
-//         $this->assets->setQuill();http://vitexopt.ru/short/9zL6pN6fL2wL
-            } else {
-                $this->assets->setMeta('Страница не найдена');
-                http_response_code(404);
-            }
-        } catch (Throwable $exception) {
+        } catch (\Throwable $exception) {
             exit($exception);
+        }
+        $this->route->setView('product');
+        if ($product) {
+            $oItems          = OrderRepository::count();
+            $breadcrumbs     = BreadcrumbsRepository::getCategoryBreadcrumbs($product->category_id, true,);
+            $userIsAdmin     = Auth::isAdmin();
+            $shippablePrices = $this->repo->dopUnitsPrices($product);
+
+            $this->set(compact('shippablePrices', 'product', 'breadcrumbs', 'oItems', 'userIsAdmin'));
+
+            $title    = $product->ownProperties->seo_title ?? $product->name;
+            $desc     = $product->ownProperties->seo_description ?? $product->name;
+            $keywords = $product->ownProperties->seo_keywords ?? $product->name;
+            $this->assets->setMeta($title, $desc, $keywords);
+
+            $this->assets->setProduct();
+//         $this->assets->setQuill();http://vitexopt.ru/short/9zL6pN6fL2wL
+        } else {
+            $this->assets->setMeta('Страница не найдена');
+            http_response_code(404);
         }
 
     }
