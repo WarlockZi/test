470ec6fb2d59d36b66da5f2432e5d9ffd4ad892d
Vitaliy new pc <vvoronik@yandex.ru>
2024-09-26 09:19:01 +0300

    fix yandex analitycs 77 ,

--- public/build/assets/card_panel-DFbdJm5i.js
+++
@@ -1,1 +0,0 @@
-import{$ as r,a as s}from"./common-DQYaDImd.js";class o{constructor(){if(this.el=r("[data-shortLink]").first(),this.el)return!1}shortLink(a){navigator.permissions.query({name:"clipboard-write"}).then(async t=>{(t.state==="granted"||t.state==="prompt")&&await navigator.clipboard.writeText(a.dataset.shortlink).then(()=>{s.show("Ссылка скопирована")})})}}export{o as default};
