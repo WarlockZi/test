418bf224140c9cac234f3ce7c21cdc46adf975e8
Vitaliy new pc <vvoronik@yandex.ru>
2024-10-27 11:14:46 +0300

    user avatar

--- app/view/Auth/yandex.php
+++ app/view/Auth/yandex.php
@@ -1,15 +1,13 @@
 <? if (!$userData): ?>
     <h1>Нет данных</h1>
 <?php else: ?>
+    <img src="https://avatars.yandex.net/get-yapic/<?= $userData['default_avatar_id'] ?>/islands-50">
     <? foreach ($userData as $key => $data): ?>
 
         <p>
             <span><strong><?= $key; ?></strong></span>
             <span> <?= $data; ?></span>
         </p>
-        <a href="https://avatars.yandex.net/get-yapic/<?= $userData['default_avatar_id'] ?>/islands-50">
-            avatar
-        </a>
 
 
     <? endforeach; ?>
