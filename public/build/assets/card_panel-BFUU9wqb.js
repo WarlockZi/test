5f78a4d8673a98ba4e1689eb1515203ae994ec8a
Vitaliy new pc <vvoronik@yandex.ru>
2024-11-26 00:31:33 +0300

    build

---
+++ public/build/assets/card_panel-BFUU9wqb.js
@@ -0,0 +1,2 @@
+import{$ as r,k as s}from"./common-DGD66q32.js";class o{constructor(){if(this.el=r("[data-shortLink]").first(),this.el)return!1}shortLink(a){navigator.permissions.query({name:"clipboard-write"}).then(async t=>{(t.state==="granted"||t.state==="prompt")&&await navigator.clipboard.writeText(a.dataset.shortlink).then(()=>{s.show("Ссылка скопирована")})})}}export{o as default};
+//# sourceMappingURL=card_panel-BFUU9wqb.js.map
