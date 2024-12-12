e07e58d889d405190ab97345b007d184ddf149ec
Vitaliy new pc <vvoronik@yandex.ru>
2024-12-12 19:37:26 +0300

    fix arg

--- app/core/Mail/consoleMail.php
+++ app/core/Mail/consoleMail.php
@@ -4,22 +4,14 @@
 //$subj = 'test';
 //$body = 'test';
 
-//$to   = $argv[1];
-//$subj = $argv[2];
-//$body = $argv[3];
-
 $to   = $argv[1];
 $subj = $argv[2];
-$body = $argv[2];
+$body = $argv[3];
 ////$headers = $argv[3];
-//
-//$to   = $_SERVER['argv'][0];
-//$subj = $_SERVER['argv'][1];
-//$body = $_SERVER['argv'][2];
+
 
 if (mail($to, $subj, $body)) {
     return true;
 } else {
     return false;
 }
-//$_SERVER['argv'][0];
