d1ccf0bbb0c121c42e7fab7b6bf562f57878268a
Vitaliy new pc <vvoronik@yandex.ru>
2024-12-12 16:24:27 +0300

    fix arg

--- app/core/Mail/ConsoleMailer.php
+++ app/core/Mail/ConsoleMailer.php
@@ -37,7 +37,7 @@ class ConsoleMailer implements Mailer
             mail($to, $subj, $body, $d);
         } else {
             try {
-                if (exec("php $path", $output)) {
+                if (exec("php -f $path", $output)) {
                     return true;
                 }
             } catch (Throwable $exception) {
