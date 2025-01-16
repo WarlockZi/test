79ba2e109769f18ee8c82f14582aae369cdf27c5
Vitaliy new pc <vvoronik@yandex.ru>
2025-01-16 13:00:24 +0300

    fix organozation microdata in header

--- app/view/Header/templates/vitex_header.php
+++ app/view/Header/templates/vitex_header.php
@@ -2,6 +2,10 @@
     <? include_once ROOT. '/app/view/share/adminPanel/adminPanel.php'; ?>
 	<div class="info" itemscope itemtype="https://schema.org/Organization">
 
+        <div class="column none">
+            <h3>Компания</h3>
+            <p itemprop="name">Витекс</p>
+        </div>
         <?php include 'logo.php'; ?>
 
         <?php include 'phone.php'; ?>
