50f28e78d48f28c65db271fd77c7542df0ff067b
Vitaliy new pc <vvoronik@yandex.ru>
2025-01-18 00:15:30 +0300

    fix clear builddd

--- public/build/assets/Like-DnoIdTG7.js
+++
@@ -1,2 +0,0 @@
-import{$ as e}from"./search-uOMQXYvZ.js";import{T as r}from"./Table-DIgBeHA6.js";class i{constructor(){const t=e("[custom-table].likes").first();if(!t)return!1;new r(t)}}export{i as default};
-//# sourceMappingURL=Like-DnoIdTG7.js.map
--- public/build/assets/card_panel-DXQJKK1o.js
+++
@@ -1,2 +0,0 @@
-import{$ as r,d as s}from"./search-uOMQXYvZ.js";class o{constructor(){if(this.el=r("[data-shortLink]").first(),this.el)return!1}shortLink(a){navigator.permissions.query({name:"clipboard-write"}).then(async t=>{(t.state==="granted"||t.state==="prompt")&&await navigator.clipboard.writeText(a.dataset.shortlink).then(()=>{s.show("Ссылка скопирована")})})}}export{o as default};
-//# sourceMappingURL=card_panel-DXQJKK1o.js.map
