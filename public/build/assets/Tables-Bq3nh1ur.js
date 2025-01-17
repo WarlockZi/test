51326045b7ec53829098770826ab86c44f9f7f20
Vitaliy new pc <vvoronik@yandex.ru>
2025-01-17 22:40:44 +0300

    fix admin tables

---
+++ public/build/assets/Tables-Bq3nh1ur.js
@@ -0,0 +1,2 @@
+import{$ as a}from"./search-uOMQXYvZ.js";import{T as e}from"./Table-DIgBeHA6.js";class l{constructor(){debugger;const o=a("[custom-table]");o&&[].forEach.call(o,function(t){new e(t)})}}export{l as default};
+//# sourceMappingURL=Tables-Bq3nh1ur.js.map
