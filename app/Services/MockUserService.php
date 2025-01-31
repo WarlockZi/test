651879443f081a786492e77ec5b1e897672845e4
Vitaliy new pc <vvoronik@yandex.ru>
2025-01-31 20:13:14 +0300

    fix mock user

--- app/Services/MockUserService.php
+++ app/Services/MockUserService.php
@@ -3,24 +3,23 @@
 namespace app\Services;
 
 use app\core\Auth;
-use \app\model\User;
+use app\model\User;
 use app\model\UserYandex;
 
 class MockUserService
 {
     public static function mockUser()
     {
-        $MarinaDemis = User::where('email', 'p.shishkov@demis.ru')
+        $MarinaDemis    = User::where('email', 'p.shishkov@demis.ru')
             ->first();
-        $Olya = User::where('email', 'vitex018@yandex.ru')
+        $Olya           = User::where('email', 'vitex018@yandex.ru')
             ->first();
-        $Oleg = User::where('email', 'molchinoleg@mail.ru')
+        $Oleg           = User::where('email', 'molchinoleg@mail.ru')
             ->first();
         $yandexVvoronik = UserYandex::where('default_email', 'vvoronik@yandex.ru')
             ->first();
 
-
-        Auth::setUser($MarinaDemis);
+            Auth::setUser($MarinaDemis);
     }
 
 }
