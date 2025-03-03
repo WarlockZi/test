d74848a0b54ae30804e9fa32cf61e2d61f005de5
Vitaliy new pc <vvoronik@yandex.ru>
2025-03-03 12:02:37 +0300

    fix add CheckboxTemplate.php data checked pivot

--- app/view/components/Builders/CheckboxBuilder/CheckboxTemplate.php
+++ app/view/components/Builders/CheckboxBuilder/CheckboxTemplate.php
@@ -1,9 +1,9 @@
 <input
-		my-checkbox
-		type="checkbox"
-	<?= $box->class; ?>
-	<?= $box->field; ?>
-	<?= $box->checked; ?>
+        my-checkbox
+        type="checkbox"
+    <?= $box['data']??''; ?>
+    <?= $box['class']??''; ?>
+    <?= $box['field']??''; ?>
+    <?= $box['pivot']??''; ?>
+    <?= $box['checked']?'checked':''; ?>
 >
-
-
