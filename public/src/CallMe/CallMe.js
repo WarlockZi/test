0e86e2c8a09846d00875d1195d262f229138cf32
TechDemis <dev@demis.ru>
2025-03-06 13:00:48 +0300

    Правки по меню на адаптиве и по оформлению текстов на страницах

--- public/src/CallMe/CallMe.js
+++ public/src/CallMe/CallMe.js
@@ -4,7 +4,7 @@ import CallMeModal from "@src/CallMe/CallMeModal.js";
 export default class CallMe{
    constructor() {
       new Modal({
-         triggers: ['#call-me', '#fixed-call-me'],
+         triggers: ['#call-me', '#fixed-call-me', '#burger-call-me'],
          boxes: new CallMeModal(),
       });
 
