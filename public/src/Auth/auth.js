ad4411ee9301489d81e652fc8bf0ec775c4ceb60
Vitaliy new pc <vvoronik@yandex.ru>
2025-03-09 09:44:00 +0300

    pretier setup, auth format

--- public/src/Auth/auth.js
+++ public/src/Auth/auth.js
@@ -1,33 +1,25 @@
-import '../components/header/Autocomplete/autocomplete'
-
-import '../components/showPassword/showPassword'
-
-import '../components/cookie/cookie'
-
-import './changepassword'
-
-import './register'
-import './profile'
-import './return_pass'
-import './edit'
-import './auth.scss'
-// import './login'
+import "../components/header/Autocomplete/autocomplete";
+import "../components/showPassword/showPassword";
+import "../components/cookie/cookie";
+import "./changepassword";
+import "./register";
+import "./profile";
+import "./return_pass";
+import "./edit";
+import "./auth.scss";
 
 import showPassword from "../components/showPassword/showPassword";
-import {$} from "../common";
-
+import { $ } from "../common";
 
 const loginForm = $("[data-auth='login']").first();
-if (loginForm){
-    const {default:Login} = await import('./login.js')
-    new Login()
+if (loginForm) {
+  const { default: Login } = await import("./login.js");
+  new Login();
 }
 
-if ($('.modal').first()){
-    const {default:Modal} = await import("../components/Modal/modal.js")
-    new Modal()
+if ($(".modal").first()) {
+  const { default: Modal } = await import("../components/Modal/modal.js");
+  new Modal();
 }
-const f= 1
 
 showPassword();
-
