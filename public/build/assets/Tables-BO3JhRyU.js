49750881e324f46f23526b8afac570fd10cab381
Vitaliy new pc <vvoronik@yandex.ru>
2025-03-11 20:43:01 +0300

    fix add assets link

---
+++ public/build/assets/Tables-BO3JhRyU.js
@@ -0,0 +1,2 @@
+import{$ as r}from"./common-DnUtA5kF.js";import{T as a}from"./Table-d1P8M7-K.js";import"./constants-D_Ps4z6O.js";import"./SelectNew-Ddq9kzI4.js";import"./search-BDRlSmDO.js";class f{constructor(){const o=r("[custom-table]");o&&[].forEach.call(o,function(t){new a(t)})}}export{f as default};
+//# sourceMappingURL=Tables-BO3JhRyU.js.map
