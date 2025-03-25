63b7148907afdc4fbbd283dc6ea2d71e5e8c9901
Vitaliy new pc <vvoronik@yandex.ru>
2025-03-25 10:37:50 +0300

    fix Storage.php sync1s path

--- app/Services/Storage/Storage.php
+++ app/Services/Storage/Storage.php
@@ -16,7 +16,7 @@ class Storage
 
     public function __construct()
     {
-        $this->path = FS::platformSlashes(ROOT . '/storage/app/');
+        $this->path = FS::platformSlashes(ROOT . '/storage/');
     }
 
     public static function getFile(string $file): string
