ff13190876d97fece61314c8cb6a2734fb06c520
TechDemis <dev@demis.ru>
2025-05-17 22:49:39 +0300

    Цели и блок Как мы работаем

--- app/view/Main/index.php
+++ app/view/Main/index.php
@@ -73,7 +73,7 @@ use app\core\Icon;
     </div>
 </div>
 
-<div class="workflow">
+<!--div class="workflow">
     <div class="workflow__title typing-animation">Как мы работаем</div>
     <div class="workflow__wrap">
         <div class="static-container">
@@ -98,5 +98,27 @@ use app\core\Icon;
             <div class="step last">Вы принимаете товар. Подписываете документы. Отправляете их нам</div>
         </div>
     </div>
-</div>
+</div-->
 
+<h2>Как мы работаем</h2>
+<div class="block_work">
+    <div class="block1">
+        <p style="margin: auto; text-align: center;"><img src="/pic/icons/21288567_user-experience_12440970_7447763.svg" class="img_main"></p>
+        <p style="margin: auto; text-align: center;">Вы звоните нам или оставляете заявку через форму на сайте</p>
+    </div>
+    <p style="margin-top: 60px;"><img src="/pic/icons/strelka.png" class="strelka"></p>
+    <div class="block1">
+        <p style="margin: auto; text-align: center;"><img src="/pic/icons/21288568_shopping-list_12441220_7447763.svg" class="img_main"></p>
+        <p style="margin: auto; text-align: center;">Выставляем счет на оплату</p>
+    </div>
+    <p style="margin-top: 60px;"><img src="/pic/icons/strelka.png" class="strelka"></p>
+    <div class="block1">
+        <p style="margin: auto; text-align: center;"><img src="/pic/icons/21288566_payment_9341320_7447763.svg" class="img_main"></p>
+        <p style="margin: auto; text-align: center;">Вы оплачиваете покупку</p>
+    </div>
+    <p style="margin-top: 60px;"><img src="/pic/icons/strelka.png" class="strelka"></p>
+    <div class="block1">
+        <p style="margin: auto; text-align: center;"><img src="/pic/icons/21288569_delivery-truck_17264164_7447763.svg" class="img_main"></p>
+        <p style="margin: auto; text-align: center;">Мы доставляем товар до клиента</p>
+    </div>
+</div>
