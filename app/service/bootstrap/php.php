c4c6b1c6e10db6eaf3a4830ecac9a0f7ff856e43
Vitaliy new pc <vvoronik@yandex.ru>
2025-07-16 12:33:27 +0300

    fix php xdebug

--- app/service/bootstrap/php.php
+++ app/service/bootstrap/php.php
@@ -1,10 +1,16 @@
 <?php
 ini_set("short_open_tag", 1);
 ini_set('memory_limit', '256M');
-ini_set('xdebug.collect_params', '0');
-ini_set('xdebug.show_local_vars  ', '0');
-ini_set('log_errors_max_len', 0);
 
+ini_set('xdebug.collect_params', '0');
+ini_set('xdebug.show_local_vars', '0');
+ini_set('xdebug.var_display_max_children', '-1');
+ini_set('xdebug.var_display_max_data', '-1');
+ini_set('xdebug.var_display_max_depth', '-1');
+
+ini_set('log_errors_max_len', 0);
+ini_set('error_prepend_string', '');
+ini_set('error_append_string', '');
 if (DEV) {
     ini_set('display_errors', 'On');
     error_reporting(E_ALL | E_STRICT);
