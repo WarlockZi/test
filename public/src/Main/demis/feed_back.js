620e4dcd5e2907f078674fe1e485ee79233db0d7
Vitaliy new pc <vvoronik@yandex.ru>
2025-10-02 15:51:12 +0300

    feat gitignore, feedback

--- public/src/Main/demis/feed_back.js
+++ public/src/Main/demis/feed_back.js
@@ -1,7 +1,9 @@
 const button = document.querySelector(".show-feedback");
 const feedbackForm = document.querySelector(".feedback-form");
 
-button.addEventListener("click", function () {
-  button.style.display = "none";
-  feedbackForm.style.display = "block";
-});
+if (!button) {
+  button.addEventListener("click", function () {
+    button.style.display = "none";
+    feedbackForm.style.display = "block";
+  });
+}
