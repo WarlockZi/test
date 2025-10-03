2bf8d42e90af5c13af38d7605436608fc9aa4312
Vitaliy new pc <vvoronik@yandex.ru>
2025-10-03 22:16:01 +0300

    CategoryRepository.php children with own properteis

--- app/repository/CategoryRepository.php
+++ app/repository/CategoryRepository.php
@@ -47,7 +47,9 @@ class CategoryRepository
                 return Category::withWhereHas(
                     'ownProperties',
                     fn($q) => $q->where('show_front', 1))
-                    ->with('childrenRecursive')
+                    ->with(['childrenRecursive'=>function($q){
+                        return $q->with('ownProperties');
+                    }])
                     ->with('ownProperties')
                     ->get()->toArray();
             },
