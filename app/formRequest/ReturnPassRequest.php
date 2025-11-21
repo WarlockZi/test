9e156e4fb235a6f797487907c516121cf31eafd5
Vitaliy new pc <vvoronik@yandex.ru>
2025-11-21 19:03:56 +0300

    fix decode image name

---
+++ app/formRequest/ReturnPassRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\formRequest;
+
+
+use app\formRequest\baseFormRequests\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use JetBrains\PhpStorm\NoReturn;
+
+
+class ReturnPassRequest extends FormRequest
+{
+    public function __construct(
+        protected $allowedFields = ['email']
+    )
+    {
+        parent::__construct();
+    }
+
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+        ];
+    }
+
+    public function all($keys = null): array
+    {
+        return $this->input;
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Заполните email.',
+            'email.email' => 'email должен быть адресом электронной почты.',
+        ];
+    }
+//    public function authorize(): bool
+//    {
+//        return isset($this->phpSession)
+//            && $this->phpSession === session_id();
+//    }
+
+}
