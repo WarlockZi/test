a368c0c0005662005e8f504e248d9fcd09172955
Vitaliy new pc <vvoronik@yandex.ru>
2025-11-24 12:37:39 +0300

    fix error blade, change pass blade, admintablestandalone

---
+++ app/blade/views/layouts/main/error.blade.php
@@ -0,0 +1,8 @@
+@if(!empty($_SESSION['error']))
+    <div class="errors">
+        <div class="message error">
+            {!!$_SESSION['error']??''!!}
+        </div>
+    </div>
+    {{ $_SESSION['error']=''}}
+@endif
