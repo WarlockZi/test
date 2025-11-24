e8e46dde642a8deef4266bf1734fd818d6830476
Vitaliy new pc <vvoronik@yandex.ru>
2025-11-24 16:40:45 +0300

    fix remove images to db

--- app/controller/MainController.php
+++ app/controller/MainController.php
@@ -27,7 +27,7 @@ class MainController extends AppController
             'Доставим нитриловые перчатки, бахилы, маски по России. Оптом.',
             'нитриловые перчатки, бахилы, маски, расходные материалы, доставка, производство, по России'
         );
-        HelpersService::setMainImages();
+//        HelpersService::setMainImages();
 
         view('main.index', compact('meta'));
     }
