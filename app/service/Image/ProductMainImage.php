2928793d9902a93740999f0e1af029ea3d94b8ce
Vitaliy new pc <vvoronik@yandex.ru>
2025-11-26 21:40:42 +0300

    fix set locale

--- app/service/Image/ProductMainImage.php
+++ app/service/Image/ProductMainImage.php
@@ -101,7 +101,7 @@ class ProductMainImage extends BaseImage
 
     public function getNameFromArt(): string
     {
-//        setlocale(LC_ALL, 'ru_RU.UTF-8', 'ru_RU', 'rus');
+        setlocale(LC_ALL, 'ru_RU.UTF-8', 'ru_RU', 'rus');
         $art = str_replace(['/', '//', '\\', '\\\\', '.', '{', '}', '$'], '_', $this->product['art']);
 //       мб такая строка "/var/www/vitexopt/data/www/vitexopt.ru/storage/app/pic/product/\xd0\x9f\xd0\x9d\xd0\x94-8_19_2\xd1\x80-\xd0\x91-\xd0\xa1_450.jpg"
         $enc =  mb_detect_encoding($art);
