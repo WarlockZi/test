258dcd309b0e961aa8a44c5ff8865206a5cdd8a2
Vitaliy new pc <vvoronik@yandex.ru>
2025-11-27 11:12:14 +0300

    fix is writable

--- app/service/Image/ProductMainImage.php
+++ app/service/Image/ProductMainImage.php
@@ -158,14 +158,14 @@ class ProductMainImage extends BaseImage
 
     public function getAbsoluteDestinationPath(): string
     {
-        $dir = FS::resolve($this->basePath . $this->productImageDir);
+        $dir = FS::resolve(ROOT, $this->basePath . $this->productImageDir);
         if (!is_writable($dir)) {
             error_log($dir. ' - закрыта для записи');
             throw new Exception('папка закрыта для записи');
         }
         $name = $this->nameFromArt;
         $type = $this->getType();
-        $path = ROOT . "$dir$name.$type";
+        $path = "$dir$name.$type";
         return $path;
     }
 
