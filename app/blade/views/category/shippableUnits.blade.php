0265d8d114eac03c7be5f267b1026db6063acd44
Vitaliy new pc <vvoronik@yandex.ru>
2026-03-13 12:09:53 +0300

    fix shippableUnits.blade.php number format

--- app/blade/views/category/shippableUnits.blade.php
+++ app/blade/views/category/shippableUnits.blade.php
@@ -41,7 +41,7 @@
 
 {{--                        <span class="contains">{!!$unit['pivot']['divider']??0!!} {!!$product['base_unit']['name']??''!!}</span>--}}
                         <div class="cost" data-cost="{{$unit['pivot']['price']??0}}">
-                            {{number_format($unit['pivot']['price'],2,'.',' ')??0}}
+                            {{$unit['pivot']['price']?number_format($unit['pivot']['price'],2,'.',' '):0}}
                         </div>
                         <div class="currency"> ₽</div>
 
