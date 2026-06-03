f8c0ba51111ba6241441ddc6b800388278749c1f
Vitaliy new pc <vvoronik@yandex.ru>
2026-06-03 16:41:56 +0300

    feat shareInfo.blade.php data_get

--- app/blade/views/admin/components/catalogItem/shareInfo.blade.php
+++ app/blade/views/admin/components/catalogItem/shareInfo.blade.php
@@ -1,15 +1,16 @@
 @if(isset($catItem['item']['own_properties']['path']))
     <div class="share-info">
         <span>адрес :: </span>
-        <a href="/{{$catItem['model']}}/{{$catItem['item']['own_properties']['path']}}">
-            {{$catItem['item']['name']??''}}
+        <a href="/{!!$catItem['model']!!}/{!!data_get($catItem,'item.own_properties.path', '')!!}">
+            {!!data_get($catItem, 'item.name', '')!!}
         </a>
     </div>
 @elseif(isset($catItem['item']['slug']))
     <div class="share-info">
         <span>адрес :: </span>
-        <a href="/{{$catItem['model']}}/{{$catItem['item']['slug']}}">
-            {{$catItem['item']['name']??''}}
+        <a href="/{!!$catItem['model']!!}/{!!data_get($catItem, 'item.slug', '')!!}">
+            {!!data_get($catItem, 'item.name', '')!!}
         </a>
     </div>
 @endif
+ 
