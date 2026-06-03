d136e6ae7cdaac6f8d1d228d33b455f95efd31ce
Vitaliy new pc <vvoronik@yandex.ru>
2026-06-03 16:36:08 +0300

    fix main_image.blade.php zoom

--- app/blade/views/product/main_image.blade.php
+++ app/blade/views/product/main_image.blade.php
@@ -2,8 +2,9 @@
 
 
     <figure class="zoom"
+            onmousemove="zoom(event)"
             style="background-image: url('{!!image($product['own_properties']['main_image'])!!}');background-repeat: no-repeat;">
-{{--        @deb--}}
+
         <img
                 src="{!!image($product['own_properties']['main_image'])!!}"
                 title='{!!$product['name']!!}'
@@ -11,5 +12,16 @@
         />
     </figure>
 </div>
+
+<script>
+   function zoom(e){
+      var zoomer = e.currentTarget;
+      e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
+      e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX;
+      x = offsetX/zoomer.offsetWidth*100;
+      y = offsetY/zoomer.offsetHeight*100;
+      zoomer.style.backgroundPosition = x + '% ' + y + '%';
+   }
+</script>
 
 
