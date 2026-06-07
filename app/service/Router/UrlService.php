56147511ea999cafcd5a53a0c69a8db69fc6d011
Vitaliy new pc <vvoronik@yandex.ru>
2026-06-07 21:28:59 +0300

    feat UrlService.php refactor

--- app/service/Router/UrlService.php
+++ app/service/Router/UrlService.php
@@ -6,28 +6,17 @@ use app\model\Category;
 
 class UrlService
 {
-//    public static function generateUrls(): void
-//    {
-//        Category::with('parent')->get()->each(function (Category $category) {
-//            self::setCategoryOwnPropPath($category);
-//        });
-//    }
 
     public static function getCategoryOwnPropPath(Category $category): string
     {
-        $path = [];
-        if (!$category->parent) {
-            return $category->slug;
-//            $res = $category->ownProperties->update(['path' => $category->slug]);
-        } else {
-            $localCategory = $category;
-            while ($category->parent) {
-                $path[]   = $category->parent->slug;
-                $category = $category->parent;
-            }
-            $str = implode('/', array_reverse($path)) . '/' . $localCategory->slug;
-            return $str;
-//            $localCategory->ownProperties->update(['path' => $str]);
+        if (!$category->parent) return $category->slug;
+
+        $path          = [];
+        $localCategory = $category;
+        while ($localCategory->parent) {
+            $path[]   = $localCategory->parent->slug;
+            $localCategory = $localCategory->parent;
         }
+        return implode('/', array_reverse($path)) . '/' . $category->slug;
     }
 }
