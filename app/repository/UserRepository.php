ac889550fa686ead8f3fc92fced6f58e8deb6d89
Vitaliy new pc <vvoronik@yandex.ru>
2026-06-07 21:27:40 +0300

    feat UserRepository.php passwordGenerator extraction

--- app/repository/UserRepository.php
+++ app/repository/UserRepository.php
@@ -6,22 +6,11 @@ namespace app\repository;
 
 use app\model\RoleUser;
 use app\model\User;
-use app\service\ShortLink\ShortlinkService;
+use app\service\PasswordGenerator\PasswordGeneratorService;
 use Illuminate\Database\Eloquent\Model;
 
 class UserRepository
 {
-    protected string $salt;
-
-    public function __construct()
-    {
-        $this->salt = "popiyonovacheesa";
-    }
-
-    public function preparePassword(string $password): string
-    {
-        return md5($password . $this->salt);
-    }
 
     public function changePassword(User $user, string $newPassword): void
     {
@@ -30,24 +19,15 @@ class UserRepository
 
     public function getByPass(string $password): User
     {
-        return User::where('password', $password)->get();
+        return User::where('password', $password)->first();
     }
 
-    public function randomPassword(): string
-    {
-        return ShortlinkService::create(8);
-    }
 
     public function getByEmail(string $email): ?User
     {
         return User::where('email', $email)
             ->select('id', 'password', 'email')
             ->first();
-    }
-
-    public function findByMail(string $mail): User
-    {
-        return User::where('email', $mail)->first();
     }
 
     public function changeRole(array $req): void
@@ -59,17 +39,13 @@ class UserRepository
             'role_id' => $roleId,
         ];
         RoleUser::query()
-            ->updateOrCreate(
-                [
-                    'user_id' => $userId,
-                ],
-                $userRole);
+            ->updateOrCreate(['user_id' => $userId,],$userRole);
     }
 
     public function createUser(array $req): Model
     {
         $user['email']    = $req['email'];
-        $user['password'] = $this->preparePassword($req['password']);
+        $user['password'] = PasswordGeneratorService::hashPassword($req['password']);
         $user['hash']     = md5(microtime());
         $user['rights']   = 'user_update';
         $user['sex']      = 'm';
