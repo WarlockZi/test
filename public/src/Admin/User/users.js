4cdcd9b03f90a0e8052ac667c819940b3b5e02bf
Vitaliy new pc <vvoronik@yandex.ru>
2026-06-12 09:48:23 +0300

    fix move DTO in users.js

--- public/src/Admin/User/users.js
+++ public/src/Admin/User/users.js
@@ -1,8 +1,7 @@
 import "./users.scss";
 import { $, post } from "@src/common.js";
 import SelectNew from "@src/components/select/SelectNew.js";
-import { ael } from "@src/constants.js";
-import DTO from "@src/Admin/DTO.js";
+import DTO from "@src/Admin/DTO/DTO.js";
 
 export default class Users {
   constructor() {
