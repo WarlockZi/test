77e968c4de99f011120fabebe1f87ca2e764c888
Vitaliy new pc <vvoronik@yandex.ru>
2026-06-24 23:38:47 +0300

    feat table.blade.php extend head

---
+++ app/blade/views/admin/components/table/head/head.blade.php
@@ -0,0 +1,10 @@
+@foreach ($data['columns'] as $c)
+    <div
+            {!!$c->headerClass??''!!}
+            {!!$c->headerSort??''!!}
+    >
+        {!!$c->headerSortIcon??''!!}
+        {!!$c->headerTitle??''!!}
+        {!!$c->headerSearch??''!!}
+    </div>
+@endforeach
