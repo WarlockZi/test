ad8b420abbc12a77328652402a363b33ea519fd9
Vitaliy new pc <vvoronik@yandex.ru>
2026-07-23 15:17:09 +0300

    debug req init

--- public/index.php
+++ public/index.php
@@ -6,7 +6,11 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
 $app = new App();
 
+if(str_contains($_SERVER['REQUEST_URI'],'init')){
+
+error_log('init------------------');
 //error_log(implodeServer());
+}
 
 function implodeServer(): string
 {
