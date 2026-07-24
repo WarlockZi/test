5bddf66a5d7b5af6cc3d95930c53c3ff5fe9f1ae
Vitaliy new pc <vvoronik@yandex.ru>
2026-07-24 15:13:19 +0300

    debug сдуфк

--- app/controller/AtestController.php
+++ app/controller/AtestController.php
@@ -3,13 +3,6 @@
 namespace app\controller;
 
 
-use app\formRequest\SyncRequest;
-use app\service\Logger\SyncLogger;
-use app\service\Sync\Load\LoadService;
-use app\service\Sync\SyncActions;
-use app\service\Sync\SyncService;
-use Throwable;
-
 class AtestController extends AppController
 {
     public function __construct(
@@ -18,19 +11,12 @@ class AtestController extends AppController
         parent::__construct();
     }
 
-
     public function actionIndex(): void
     {
-        try {
             error_log('atest start');
-//            $this->logger->write('test start');
-//            error_log('after log');
 //            $services = new SyncService(new LoadService(), new SyncLogger(), new SyncActions(new SyncLogger()));
 //            $logger = new SyncLogger();
             error_log('atest new serv ');
 //            $services->requestFrom1s();
-        } catch (Throwable $exception) {
-            error_log('atest'.$exception);
-        }
     }
 }
