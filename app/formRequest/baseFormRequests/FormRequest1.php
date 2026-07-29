ac6235fcd8916248cef6972ddbd4e32bf84c12e5
Vitaliy new pc <vvoronik@yandex.ru>
2026-07-29 22:44:25 +0300

    debug FormRequest1.php attr messsafe

--- app/formRequest/baseFormRequests/FormRequest1.php
+++ app/formRequest/baseFormRequests/FormRequest1.php
@@ -76,7 +76,7 @@ abstract class FormRequest1 extends Request
         }
 
         if ($validator->fails()) {
-            $errors = $validator->errors()->all('string');
+            $errors = $validator->errors()->all(':attribute: :message');
             response()->json(['popup' => $errors]);
 //            response()->json(['popup' => $this->errorsToString($errors)]);
         }
