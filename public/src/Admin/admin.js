4c2cf50989e56e9d4e7389acef93eddd272c51a5
Vitaliy new pc <vvoronik@yandex.ru>
2026-08-04 13:26:40 +0300

    fix load module false del adm

--- public/src/Admin/admin.js
+++ public/src/Admin/admin.js
@@ -58,7 +58,7 @@ $(document).ready(async function () {
 
     const moduleName = $(`[data-jsmodule]`).first()?.dataset?.jsmodule;
 
-    const modulePath = `@src/Admin/Pages/${moduleName}/${moduleName}.js`;
+    const modulePath = `/Admin/Pages/${moduleName}/${moduleName}.js`;
     const moduleData = modules[modulePath];
 
     if (!moduleData) {
